7016ac1897b35a18066c7614e6bba3aaa0d00034
gilang sandhika putra <gilangsandhika_putra@yahoo.com>
2023-02-14 19:48:40 +0700

    minor update

--- space/connections.php
+++ space/connections.php
@@ -1,12 +1,15 @@
 <?php
+    $dbhost = "0.0.0.0:3306";
+    $dbuser = "root";
+    $dbpass = "root";
     $dbname = "space";
-    $connection = mysqli_connect("localhost", "root", "", $dbname);
+    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
     
     $spacedir = "assets/";
 
     $API = "https://eoaq73qseezwdn9.m.pipedream.net";
 
-    $server = $_SERVER['SERVER_NAME'] . "/rose2";
+    $server = $_SERVER['SERVER_NAME'] . "/rose";
     $defaultuser = "Administrator";
     $defaultpass = "2022";
 ?>

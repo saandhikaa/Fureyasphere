d2aa374f90671bab1c4faaf1fedd6fe574d14752
saandhikaa <gilangsandhika_putra@yahoo.com>
2023-06-25 08:57:04 +0700

    add: create and connect to database

--- core/DatabaseConnection.php
+++ core/DatabaseConnection.php
@@ -4,12 +4,14 @@
         private $host;
         private $username;
         private $password;
+        private $database;
         private $conn;
     
-        public function __construct($host, $username, $password) {
+        public function __construct($host, $username, $password, $database) {
             $this->host = $host;
             $this->username = $username;
             $this->password = $password;
+            $this->database = $database;
         }
     
         public function connect() {
@@ -20,6 +22,25 @@
                 die("<br>Connection failed: " . $this->conn->connect_error);
             } else {
                 echo "<br>Connected to server ($this->host)";
+            }
+            
+            $query = "CREATE DATABASE IF NOT EXISTS $this->database";
+            
+            if ($this->conn->query($query) === TRUE) {
+                echo "<br>Database ($this->database) created successfully";
+            } else {
+                echo "<br>Error creating database: " . $this->conn->error;
+            }
+            
+            $this->closeConnection();
+            
+            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
+            
+            // Check connection
+            if ($this->conn->connect_error) {
+                die("<br>Connection failed: " . $this->conn->connect_error);
+            } else {
+                echo "<br>Connected to database ($this->database)";
             }
         }
     

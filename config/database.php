1555fe8b21d3c447d71c62dc83f679f7f86ed63d
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-06-27 22:01:16 +0700

    remove unnecessary variable and rename the global variable

--- config/database.php
+++ config/database.php
@@ -1,15 +1,6 @@
 <?php
-    $dbhost = "0.0.0.0:3306";
+    $host = "0.0.0.0:3306";
     $dbuser = "root";
     $dbpass = "root";
-    $dbname = "space";
-    $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-    
-    $spacedir = "assets/";
-
-    $API = "https://eoaq73qseezwdn9.m.pipedream.net";
-
-    $server = $_SERVER['SERVER_NAME'] . "/rose";
-    $defaultuser = "Administrator";
-    $defaultpass = "2022";
+    $database = "clouds";
 ?>

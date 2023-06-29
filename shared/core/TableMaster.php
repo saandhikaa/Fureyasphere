e42fa7dfd8da4de7528778f4068886efe185b0d9
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-06-29 21:17:19 +0700

    create method: show all table

--- shared/core/TableMaster.php
+++ shared/core/TableMaster.php
@@ -9,5 +9,18 @@
             $this->database = $database;
             $this->queryExecution = $queryExecution;
         }
+        
+        public function showAll() {
+            $tables = array();
+            $t = $this->queryExecution->executeQuery("SHOW TABLES");
+            for ($nt = 0; $nt < count($t); $nt++) {
+                $table = $t[$nt]["Tables_in_" . $this->database];
+                $h = $this->queryExecution->executeQuery("SHOW COLUMNS FROM " . $table);
+                for ($nh = 0; $nh < count($h); $nh++) {
+                    $tables[$table][$nh] = $h[$nh]["Field"];
+                }
+            }
+            return $tables;
+        }
     }
 ?>

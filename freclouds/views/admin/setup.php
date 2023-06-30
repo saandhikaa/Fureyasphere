175a0e704f41fe3644493e00f865d9bcc123cd27
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-06-30 14:47:13 +0700

    move to admin/ and change some type and length a column

--- freclouds/views/admin/setup.php
+++ freclouds/views/admin/setup.php
@@ -8,24 +8,23 @@
                 array(
                     "cloudfiles",
                     "CREATE TABLE cloudfiles (
-                    id INT(10) PRIMARY KEY, 
-                    owner_ VARCHAR(20) NOT NULL,
-                    codename_ VARCHAR(20) NOT NULL, 
-                    sector_ VARCHAR(2) NOT NULL, 
-                    name_ VARCHAR(50) NOT NULL,
-                    savedname_ VARCHAR(71) NOT NULL,
-                    size_ VARCHAR(11) NOT NULL,
-                    hours_ INT(4) NOT NULL
+                    id INT(10) PRIMARY KEY,
+                    owner_ INT(10) NOT NULL,
+                    codename_ VARCHAR(20) NOT NULL,
+                    key_ INT(2) NOT NULL,
+                    filename_ VARCHAR(100) NOT NULL,
+                    size_ INT(11) NOT NULL,
+                    duration_ INT(4) NOT NULL
                     )"
                 ),
                 array(
                     "logging",
                     "CREATE TABLE logging ( 
-                    id INT(10) PRIMARY KEY, 
-                    owner_ VARCHAR(20) NOT NULL, 
-                    codename_ VARCHAR(20) NOT NULL, 
-                    name_ VARCHAR(255) NOT NULL, 
-                    size_ VARCHAR(11) NOT NULL 
+                    id INT(10) PRIMARY KEY,
+                    owner_ INT(10) NOT NULL,
+                    codename_ VARCHAR(20) NOT NULL,
+                    filename_ VARCHAR(100) NOT NULL,
+                    size_ INT(11) NOT NULL
                     )"
                 )
             );

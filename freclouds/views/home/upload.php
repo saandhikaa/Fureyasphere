cf7a69a661141611032e0e55fc93b885f644ef2f
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-01 15:43:48 +0700

    add codename field

--- freclouds/views/home/upload.php
+++ freclouds/views/home/upload.php
@@ -8,6 +8,10 @@
     <form action="" method="post" enctype="multipart/form-data">
         <label for="file">open file: </label>
         <input type="file" id="file" name="file">
+        <br>
+        <label for="codename">codename: </label>
+        <input type="text" id="codename" name="codename">
+        <br>
         <input type="submit" name="submit" value="Upload">
     </form>
 </div>

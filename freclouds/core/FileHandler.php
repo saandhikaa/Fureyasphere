605c9816395e510aca9180bdd965bff0e83c2662
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-01 14:21:05 +0700

    create method: upload

--- freclouds/core/FileHandler.php
+++ freclouds/core/FileHandler.php
@@ -1,10 +1,25 @@
 <?php
     class FileHandler {
         private $queryExecution; 
+        private $path = __DIR__ . "/../uploads/";
         
         public function __construct() {
             global $queryExecution;
             $this->queryExecution = $queryExecution;
+        }
+        
+        public function upload () {
+            $uploadtime = time();
+            
+            if ($_FILES['file']['error'] != 0) {
+                echo '<script>alert("File upload failed: error code (' . $_FILES['file']['error'] . ')"); window.location.href = "";</script>';
+                exit; 
+            }
+            
+            if (!move_uploaded_file($_FILES['file']['tmp_name'], $this->path . $uploadtime . "_" . $_FILES["file"]["name"])) {
+                echo '<script>alert("Failed to save file"); window.location.href = "";</script>';
+                exit;
+            }
         }
     }
 ?>

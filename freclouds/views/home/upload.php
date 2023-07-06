6f4b2f3971a1ce1986203eb6ed1dfe2b6e8ff1a7
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-06 09:42:11 +0700

    add attribute: required

--- freclouds/views/home/upload.php
+++ freclouds/views/home/upload.php
@@ -10,12 +10,12 @@
     <form action="" method="post" enctype="multipart/form-data">
         <div id="file-upload-container">
             <div class="file-input-container">
-                <input type="file" name="file[]" class="file-input" onchange="handleFileSelection(this)">
+                <input type="file" name="file[]" class="file-input" onchange="handleFileSelection(this)" required>
             </div>
         </div>
         <br>
         <label for="codename">codename: </label>
-        <input type="text" id="codename" name="codename">
+        <input type="text" id="codename" name="codename" required>
         <br>
         <br>
         <input type="submit" name="submit" value="Upload">

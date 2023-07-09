c84eb14ecb7dc4e9cd347429974e38666c4f11f2
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-09 11:23:31 +0700

    handling multiple uploaded files

--- freclouds/core/FileHandler.php
+++ freclouds/core/FileHandler.php
@@ -14,8 +14,6 @@
             $owner = "anonymous";
             $codename = htmlspecialchars(rtrim($_POST["codename"]));
             $key = rand(10,99);
-            $filename = $_FILES["file"]["name"];
-            $size = $_FILES["file"]["size"];
             $duration = 1;
             
             // regenerate key for duplicate codename
@@ -25,25 +23,32 @@
                 $key = rand(10, 99);
             }
             
-            // processing file
-            if ($_FILES['file']['error'] != 0) {
-                echo '<script>alert("File upload failed: error code (' . $_FILES['file']['error'] . ')"); window.location.href = "";</script>';
-                exit; 
+            foreach ($_FILES['file']['error'] as $index => $error) {
+                if ($error == 0) {
+                    $filename = $_FILES['file']['name'][$index];
+                    $size = $_FILES['file']['size'][$index];
+                    
+                    // saving file
+                    if (!move_uploaded_file($_FILES['file']['tmp_name'][$index], $this->path . $uploadtime . "_" . $filename)) {
+                        echo '<script>alert("Failed to save file"); window.location.href = "";</script>';
+                        exit;
+                    }
+                    
+                    // create on database table cloudfiles
+                    $query = "INSERT INTO cloudfiles (id, owner_, codename_, key_, filename_, size_, duration_) VALUES ('$uploadtime', '$owner', '$codename', '$key', '$filename', '$size', '$duration')";
+                    if (!$this->queryExecution->executeQuery($query)) {
+                        echo '<script>alert("Failed to insert into cloudfiles"); window.location.href = "";</script>';
+                        exit;
+                    }
+                    
+                    // create on database table logging
+                    $query = "INSERT INTO logging (id, owner_, codename_, filename_, size_) VALUES ('$uploadtime', '$owner', '$codename', '$filename', '$size')";
+                    if (!$this->queryExecution->executeQuery($query)) {
+                        echo '<script>alert("Failed to insert into logging"); window.location.href = "";</script>';
+                        exit;
+                    }
+                }
             }
-            
-            // saving file
-            if (!move_uploaded_file($_FILES['file']['tmp_name'], $this->path . $uploadtime . "_" . $_FILES["file"]["name"])) {
-                echo '<script>alert("Failed to save file"); window.location.href = "";</script>';
-                exit;
-            }
-            
-            // create on database table cloudfiles
-            $query = "INSERT INTO cloudfiles (id, owner_, codename_, key_, filename_, size_, duration_) VALUES ('$uploadtime', '$owner', '$codename', '$key', '$filename', '$size', '$duration')";
-            $this->queryExecution->executeQuery($query);
-            
-            // create on database table logging
-            $query = "INSERT INTO logging (id, owner_, codename_, filename_, size_) VALUES ('$uploadtime', '$owner', '$codename', '$filename', '$size')";
-            $this->queryExecution->executeQuery($query);
         }
     }
 ?>

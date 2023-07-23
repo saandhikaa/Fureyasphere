f9f42cd991c80775ab41ef7ff9ed0cf1289d5a00
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-23 14:41:21 +0700

    remove unused line

--- index.php
+++ index.php
@@ -1,19 +1,3 @@
 <?php
-    require_once __DIR__ . "/include/init.php";
+    require_once __DIR__ . "/shared/init.php";
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FRE CLOUDS</title>
-    <script src="assets/js/main.js"></script>
-</head>
-    
-<body>
-    <br>
-    <?php require_once "views/home/upload.php" ?>
-</body>
-</html>

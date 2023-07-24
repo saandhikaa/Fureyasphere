05301329e45ff3e892a39808044ea9cc5f99a1e9
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-24 09:14:16 +0700

    add some properties

--- shared/core/App.php
+++ shared/core/App.php
@@ -1,5 +1,10 @@
 <?php
     class App {
+        protected $controller = 'Home';
+        protected $method = 'index';
+        protected $params = array();
+        protected $controllerDir = array(__DIR__ . "/../controllers/");
+    
         public function parseURL() {
             if (isset($_GET['url'])){
                 $url = rtrim($_GET['url'], '/');

8af7f844ada5171293f8d64bce36db05621f8f25
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-24 09:12:10 +0700

    create class App with method parseURL

---
+++ shared/core/App.php
@@ -0,0 +1,12 @@
+<?php
+    class App {
+        public function parseURL() {
+            if (isset($_GET['url'])){
+                $url = rtrim($_GET['url'], '/');
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                $url = explode('/', $url);
+                return $url;
+            }
+        }
+    }
+?>

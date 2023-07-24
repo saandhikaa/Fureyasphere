6d107bd0fe92376cf8fec95307858449c1eecdc1
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-24 09:02:45 +0700

    update initiation path

--- shared/init.php
+++ shared/init.php
@@ -1,23 +1,12 @@
 <?php
     // Autoloader function
     spl_autoload_register(function($className){
-        $classFile = __DIR__ . '/../core/' . $className . '.php';
+        $classFile = __DIR__ . '/core/' . $className . '.php';
         if (file_exists($classFile)) {
             require_once $classFile;
         }
     });
     
     // load config
-    require_once __DIR__ . "/../../config/database.php";
-    
-    // Create an instance of the DatabaseConnection class
-    $dbConnection = new DatabaseConnection($host, $dbuser, $dbpass, $database);
-    
-    // Connect to the database
-    $dbConnection->connect();
-    
-    // Create an instance of the QueryExecution class
-    $queryExecution = new QueryExecution($dbConnection);
-    
-    $tableMaster = new TableMaster();
+    require_once __DIR__ . "/config/config.php";
 ?>

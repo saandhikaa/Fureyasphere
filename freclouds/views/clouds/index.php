ebcb1b8cf97c4158f07daeb08ecb11360860d97b
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-26 13:30:09 +0700

    create sample index views

---
+++ freclouds/views/clouds/index.php
@@ -0,0 +1,9 @@
+<div>
+    <form action="" method="post">
+        <label for="codename">codename: </label><br/>
+        <input type="text" id="codename" name="codename" required><br/>
+        <label for="key">key: </label><br/>
+        <input type="text" id="key" name="key" required><br/><br/>
+        <input type="submit" name="action" value="Download">
+    </form>
+</div>

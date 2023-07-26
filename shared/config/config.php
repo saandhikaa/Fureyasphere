2bf5a4932afa233abef2af7d6161739184d42aff
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-26 19:11:15 +0700

    update BASEURL

--- shared/config/config.php
+++ shared/config/config.php
@@ -1,5 +1,5 @@
 <?php
-    define('BASEURL', 'http://127.0.0.1:8080');
+    define('BASEURL', 'http://127.0.0.1:8080/fureya-cloud-service');
     
     define('DB_HOST', '127.0.0.1:3306');
     define('DB_USER', 'root');

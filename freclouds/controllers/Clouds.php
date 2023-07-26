92c1bcd19170b46596b97d487e57db4053201d80
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-26 13:27:10 +0700

    add properties data and call templates views

--- freclouds/controllers/Clouds.php
+++ freclouds/controllers/Clouds.php
@@ -3,7 +3,13 @@
         private $app = "freclouds";
         
         public function index() {
+            $data = array(
+                "title" => ucwords($this->app)
+            );
+            
+            $this->view("shared", "templates/header", $data);
             $this->view($this->app, "clouds/index");
+            $this->view("shared", "templates/footer");
         }
     }
 ?>

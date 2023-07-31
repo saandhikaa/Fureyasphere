c3a24908b413fab5a942477fe0b2fdfcc9655a81
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-07-31 23:47:30 +0700

    change array defined

--- fuclouds/controllers/Clouds.php
+++ fuclouds/controllers/Clouds.php
@@ -1,23 +1,20 @@
 <?php
     class Clouds extends Controller {
         private $app = "fuclouds";
+        private $data;
         
         public function index() {
-            $data = array(
-                "title" => ucwords($this->app) . ": Search"
-            );
+            $this->data["title"] = ucwords($this->app) . ": Search";
             
-            $this->view("shared", "templates/header", $data);
+            $this->view("shared", "templates/header", $this->data);
             $this->view($this->app, "clouds/index");
             $this->view("shared", "templates/footer");
         }
         
         public function upload() {
-            $data = array(
-                "title" => ucwords($this->app) . ": Upload"
-            );
+            $this->data["title"] = ucwords($this->app) . ": Upload";
             
-            $this->view("shared", "templates/header", $data);
+            $this->view("shared", "templates/header", $this->data);
             $this->view($this->app, "clouds/upload");
             $this->view("shared", "templates/footer");
         }

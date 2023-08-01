7fe6d40c87673acf816a97c11e69cde98431957f
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-01 20:20:17 +0700

    run additional script if available

--- shared/views/templates/footer.php
+++ shared/views/templates/footer.php
@@ -1,3 +1,10 @@
-
+<?php
+    if (isset($data["appScript"])) {
+        echo $data["appScript"];
+    }
+    if (isset($data["runScript"])) {
+        echo $data["runScript"];
+    }
+?>
 </body>
 </html>

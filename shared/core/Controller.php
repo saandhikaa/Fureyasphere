d025556039c9035860087d6d0d56a51dc2ef1192
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-02 15:40:53 +0700

    create model method for handling model

--- shared/core/Controller.php
+++ shared/core/Controller.php
@@ -3,5 +3,10 @@
         public function view ($app, $view, $data = array()) {
             require_once __DIR__ . "/../../" . $app . "/views/" . $view . ".php";
         }
+        
+        public function model ($app, $model) {
+            require_once __DIR__ . "/../../" . $app . "/models/" . $model . ".php";
+            return new $model;
+        }
     }
 ?>

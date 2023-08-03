583ba87687d3dbacb550183fa8c74ea65d903594
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-03 13:29:45 +0700

    tranform into PDO, create db instance in __construct

--- shared/models/TableMaster.php
+++ shared/models/TableMaster.php
@@ -1,32 +1,9 @@
 <?php
     class TableMaster {
-        private $database, $queryExecution;
+        private $db;
         
         public function __construct() {
-            global $database, $queryExecution; 
-            $this->database = $database;
-            $this->queryExecution = $queryExecution;
-        }
-        
-        public function getList() {
-            $tables = array();
-
-            foreach ($this->queryExecution->executeQuery("SHOW TABLES", TRUE) as $tableData) {
-                $table = $tableData["Tables_in_" . $this->database];
-                foreach ($this->queryExecution->executeQuery("SHOW COLUMNS FROM " . $table, TRUE) as $columnData) {
-                    $tables[$table][] = $columnData["Field"];
-                }
-            }
-            
-            return $tables;
-        }
-        
-        public function create ($sql) {
-            $this->queryExecution->executeQuery($sql);
-        }
-        
-        public function drop ($table) {
-            $this->queryExecution->executeQuery("DROP TABLE " . $table);
+            $this->db = new Database;
         }
     }
 ?>

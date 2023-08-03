807f8f4df34e12f5d8fe693a3bfe3748d66d236e
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-03 13:31:53 +0700

    create method: get table structure

--- shared/models/TableMaster.php
+++ shared/models/TableMaster.php
@@ -5,5 +5,16 @@
         public function __construct() {
             $this->db = new Database;
         }
+        
+        public function getTableStructure ($tableName) {
+            $query = "SHOW COLUMNS FROM $tableName";
+            try {
+                $this->db->query($query);
+                $this->db->execute();
+                return $this->db->result(true);
+            } catch (PDOException $e) {
+                return false;
+            }
+        }
     }
 ?>

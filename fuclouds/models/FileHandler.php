6b8e2618e9d475e1db66eeb810d97c35619b9b81
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-04 14:37:45 +0700

    create method: handleDuplicate

--- fuclouds/models/FileHandler.php
+++ fuclouds/models/FileHandler.php
@@ -9,11 +9,11 @@
         
         public function upload() {
             $accepted = $this->slice();
-            print_r($accepted);
+            $filename = $this->handleDuplicate($accepted["name"]);
             
             for ($i = 0; $i < count($accepted["name"]); $i++) {
                 if ($accepted["error"][$i] === 0) {
-                    move_uploaded_file($accepted["tmp_name"][$i], $this->path . $accepted["name"][$i]); 
+                    move_uploaded_file($accepted["tmp_name"][$i], $this->path . $filename[$i]); 
                 }
             }
         }
@@ -31,6 +31,23 @@
                 }
             }
             return $sliced;
+        }
+        
+        public function handleDuplicate ($array) {
+            $counts = array_count_values($array);
+        
+            foreach ($array as $key => $filename) {
+                if ($counts[$filename] > 1) {
+                    $baseName = pathinfo($filename, PATHINFO_FILENAME);
+                    $extension = pathinfo($filename, PATHINFO_EXTENSION);
+                    $count = $counts[$filename];
+                    $counts[$filename]--;
+                    $newFilename = $baseName . '_' . sprintf('%02d', $count) . '.' . $extension;
+                    $array[$key] = $newFilename;
+                }
+            }
+        
+            return $array;
         }
     }
 ?>

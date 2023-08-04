4ef563a26e1ad4d7e31a4ff5155f20e84f4dc1b2
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-04 07:20:23 +0700

    create method: slice

--- fuclouds/models/FileHandler.php
+++ fuclouds/models/FileHandler.php
@@ -5,5 +5,20 @@
         public function __construct() {
             $this->db = new Database;
         }
+        
+        // cut canceled file
+        public function slice() {
+            $sliced = [];
+            foreach ($_FILES['file']['name'] as $index => $file) {
+                if (in_array($file, $_POST['post'])) {
+                    $sliced['name'][] = $_FILES['file']['name'][$index];
+                    $sliced['type'][] = $_FILES['file']['type'][$index];
+                    $sliced['size'][] = $_FILES['file']['size'][$index];
+                    $sliced['error'][] = $_FILES['file']['error'][$index];
+                    $sliced['tmp_name'][] = $_FILES['file']['tmp_name'][$index];
+                }
+            }
+            return $sliced;
+        }
     }
 ?>

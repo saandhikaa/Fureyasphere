f0d8bb8ec602914fa9e4fdf463c5c9dc02f604b6
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-07 11:40:50 +0700

    add hidden input token

--- fuclouds/views/clouds/index.php
+++ fuclouds/views/clouds/index.php
@@ -4,6 +4,7 @@
         <input type="text" id="codename" name="codename" required><br/>
         <label for="key">key: </label><br/>
         <input type="text" id="key" name="key" required><br/><br/>
+        <input type="hidden" name="token" value="<?= SR_TOKEN ?>">
         <input type="submit" name="action" value="Search" id="search" onclick="updateURL()">
     </form>
     <br/>

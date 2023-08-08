3efaecbbe8aa343b0b6a2c825cea429ee1935089
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-08 07:10:50 +0700

    add properties data

--- shared/controllers/Home.php
+++ shared/controllers/Home.php
@@ -1,7 +1,11 @@
 <?php
     class Home extends Controller {
+        private $data = [];
+        
         public function index() {
-            $this->view("shared", "templates/header");
+            $this->data["title"] = "FUREYA CLOUDS SERVICE";
+            
+            $this->view("shared", "templates/header", $this->data);
             $this->view("shared", "home/index");
             $this->view("shared", "templates/footer");
         }

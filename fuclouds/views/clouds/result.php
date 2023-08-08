afe6d88dc110bdfe75d59a939a5e577089840438
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-08 20:21:40 +0700

    add form to download file

--- fuclouds/views/clouds/result.php
+++ fuclouds/views/clouds/result.php
@@ -1,11 +1,21 @@
-<?php
-    var_dump($data);
-?>
-
 <div>
     <ul>
         <?php foreach ($data["result"]["files"] as $file): ?>
-            <li>Filename: <?php echo $file["filename_"]; ?>, Filesize: <?php echo $file["filesize_"]; ?></li>
+            <li>
+                <div>
+                    <p><?= $file["filename_"] ?></p>
+                    <p><?= $file["filesize_"] ?></p>
+                </div>
+                <form action="<?= BASEURL ?>/Clouds/result" method="post">
+                    <?php $filepath = $file["time_"] . "_" . $file["filename_"] ?>
+                    <input type="hidden" name="filename" value="<?= $file['filename_'] ?>">
+                    <input type="hidden" name="filepath" value="<?= $filepath ?>">
+                    <input type="hidden" name="codename" value="<?= $file['codename_'] ?>">
+                    <input type="hidden" name="key" value="<?= $file['key_'] ?>">
+                    <input type="hidden" name="token" value="<?= DL_TOKEN ?>">
+                    <input type="submit" value="Download">
+                </form>
+            </li>
         <?php endforeach; ?>
     </ul>
 </div>

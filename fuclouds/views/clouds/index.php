85380ab19fc8f5bb3f675e88f3ba3c51f64dd430
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-10 14:32:20 +0700

    add attribute oninput validateInput

--- fuclouds/views/clouds/index.php
+++ fuclouds/views/clouds/index.php
@@ -1,7 +1,7 @@
 <div>
     <form action="<?= BASEURL ?>/Clouds/result" method="post" id="searching">
         <label for="codename">codename: </label><br/>
-        <input type="text" id="codename" name="codename" required><br/>
+        <input type="text" id="codename" name="codename" oninput="validateInput(this)" required><br/>
         <label for="key">key: </label><br/>
         <input type="text" id="key" name="key" required><br/><br/>
         <input type="hidden" name="token" value="<?= SR_TOKEN ?>">

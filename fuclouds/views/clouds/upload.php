4220f0cc168e806045815d69691a5486af4588a2
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-11 11:16:36 +0700

    move staged-list out from file-upload-container

--- fuclouds/views/clouds/upload.php
+++ fuclouds/views/clouds/upload.php
@@ -1,9 +1,8 @@
 <div class="upload">
     
     <form action="<?= BASEURL ?>/Clouds/result" method="post" enctype="multipart/form-data">
-        <div id="file-upload-container">
-            <ul id="staged-files"></ul>
-        </div>
+        <ul id="staged-files"></ul>
+        <div id="file-upload-container"></div>
         <br>
         <label for="codename">codename: </label>
         <input type="text" id="codename" name="codename" oninput="validateInput(this)" required>

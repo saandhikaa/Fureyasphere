e73158f6635909afe6556762613d1271b51b1123
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-12 08:15:56 +0700

    add button browse files

--- fuclouds/views/clouds/upload.php
+++ fuclouds/views/clouds/upload.php
@@ -4,6 +4,9 @@
         <ul id="staged-files"></ul>
         <div id="file-upload-container"></div>
         <br>
+        <button type="button" onclick="clickActiveInput()">Browse file</button>
+        <br>
+        <br>
         <label for="codename">codename: </label>
         <input type="text" id="codename" name="codename" oninput="validateInput(this)" required>
         <br>

5a6a41029cdc505716f20731ca68c0d6cbd18918
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-13 07:30:37 +0700

    turn off autocomplete

--- fuclouds/views/clouds/upload.php
+++ fuclouds/views/clouds/upload.php
@@ -8,7 +8,7 @@
         <br>
         <br>
         <label for="codename">codename: </label>
-        <input type="text" id="codename" name="codename" oninput="validateInput(this)" required>
+        <input type="text" id="codename" name="codename" oninput="validateInput(this)" autocomplete="off" required>
         <br>
         <br>
         <input type="hidden" name="token" value="<?= UP_TOKEN ?>">

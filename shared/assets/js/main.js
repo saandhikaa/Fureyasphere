6cc37c1434b58a0f0c2c053588a836f74582a7e5
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-16 15:11:14 +0700

    create function passwordStatus match/not

--- shared/assets/js/main.js
+++ shared/assets/js/main.js
@@ -1,3 +1,15 @@
+function passwordStatus() {
+    const password = document.querySelector('#password');
+    const confirmPassword = document.querySelector('#confirm-password');
+    const passwordMatchStatus = document.querySelector('#password-match-status');
+
+    if (password.value === confirmPassword.value) {
+        passwordMatchStatus.textContent = 'Passwords match';
+    } else {
+        passwordMatchStatus.textContent = 'Passwords do not match';
+    }
+}
+
 function toggleVisibility(id) {
     const input = document.querySelector(`#${id}`);
     const button = document.querySelector(`#toggle-${id}-visibility`);

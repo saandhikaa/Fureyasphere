eddfd76bb22cf7e41673efee679888f96fb16166
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-17 10:18:43 +0700

    create method: usernameavailability

--- shared/controllers/Users.php
+++ shared/controllers/Users.php
@@ -1,23 +1,34 @@
 <?php
-   class Users extends Controller {
-       private $app = "shared";
-       private $data = [];
-       
+    class Users extends Controller {
+        private $app = "shared";
+        private $data = [];
+        
         public function __construct() {
             $this->data["styles"] = '<link rel="stylesheet" href="' . BASEURL . '/' . $this->app . '/assets/css/style.css">';
             $this->data["appScript"] = '<script src="' . BASEURL . '/' . $this->app . '/assets/js/main.js"></script>';
         }
         
-       public function index() {
-           
-       }
-       
-       public function registration() {
-           $this->data["title"] = "Registration";
-           
-           $this->view($this->app, "templates/header", $this->data);
-           $this->view($this->app, "users/registration");
-           $this->view($this->app, "templates/footer", $this->data);
-       }
-   }
+        public function index() {
+            
+        }
+        
+        public function registration() {
+            $this->data["title"] = "Registration";
+            
+            $this->view($this->app, "templates/header", $this->data);
+            $this->view($this->app, "users/registration");
+            $this->view($this->app, "templates/footer", $this->data);
+        }
+        
+        // Handle the AJAX request
+        public function usernameavailability() {
+            if (isset($_POST["username"])) {
+                if (checkUsername($_POST["username"])) {
+                    echo "available";
+                } else {
+                    echo "taken";
+                }
+            }
+        }
+    }
 ?>

1cbff0e16ece1e7e0e7dc79ba94a710e71abcb4e
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-17 10:19:44 +0700

    create function checkUsernameAvailability

--- shared/assets/js/main.js
+++ shared/assets/js/main.js
@@ -27,3 +27,41 @@ function toggleVisibility(id) {
         button.textContent = 'Show';
     }
 }
+
+var typingTimer;
+var doneTypingInterval = 1000; // 1 second
+
+function checkUsernameAvailability() {
+    var usernameInput = document.getElementById("username");
+    var messageElement = document.getElementById("username-message");
+    var username = usernameInput.value;
+
+    // Clear the previous timer
+    clearTimeout(typingTimer);
+    messageElement.textContent = "";
+    
+    // Start the timer after the user stops typing for a certain interval
+    if (username.length > 3) {
+        typingTimer = setTimeout(function() {
+            // Perform AJAX request to the server
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", "http://127.0.0.1:8080/fureya-cloud-service/Users/usernameavailability", true);
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    if (xhr.responseText === "taken") {
+                        messageElement.textContent = "Already taken.";
+                        messageElement.style.color = redhex;
+                    } else {
+                        messageElement.textContent = "Available.";
+                        messageElement.style.color = greenhex;
+                    }
+                }
+            };
+            xhr.send("username=" + encodeURIComponent(username));
+        }, doneTypingInterval);
+    } else {
+        // Clear the message and reset the color
+        messageElement.textContent = "";
+    }
+}

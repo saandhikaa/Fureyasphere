e347dbc349dd5c8a712de5d9af392c6d95c6934e
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-17 07:19:30 +0700

    load styles if available

--- shared/views/templates/header.php
+++ shared/views/templates/header.php
@@ -5,6 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $data["title"] ?></title>
+    <?php
+        if (isset($data["styles"])) {
+            echo $data["styles"];
+        }
+    ?>
 </head>
     
 <body>

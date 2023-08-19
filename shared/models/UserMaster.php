73d0cfce88223a8333e23776c74546e89e59a736
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-19 07:56:32 +0700

    fix model method: checkUsername

--- shared/models/UserMaster.php
+++ shared/models/UserMaster.php
@@ -1,18 +1,18 @@
 <?php
     class UserMaster {
         private $db;
+        private $table = "users";
         
         public function __construct() {
             $this->db = new Database;
         }
         
         public function checkUsername ($username) {
-            $query = "SELECT COUNT(*) FROM users WHERE username = :username";
+            $query = "SELECT username_ FROM $this->table WHERE username_ = :username";
             $this->db->query($query);
             $this->db->bind(':username', $username);
-            $this->db->execute();
             
-            return $this->db->rowCount() === 0;
+            return empty($this->db->result());
         }
     }
 ?>

348dce96b693df6158cc87e3e78ff0cbdc3e45c1
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-20 08:12:06 +0700

    create model method: signUp

--- shared/models/UserMaster.php
+++ shared/models/UserMaster.php
@@ -14,5 +14,23 @@
             
             return empty($this->db->result());
         }
+        
+        public function signUp ($username, $password) {
+            if (!$this->checkUsername($username)) {
+                return ($this->signIn($username, $password));
+            }
+            
+            // Hash the password using password_hash function
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+            
+            $this->db->query("INSERT INTO $this->table (time_, username_, password_, level_) VALUES (:time, :username, :password, :level)");
+            $this->db->bind(':time', time());
+            $this->db->bind(':username', $username);
+            $this->db->bind(':password', $hashedPassword);
+            $this->db->bind(':level', '1');
+            $this->db->execute();
+            
+            return $this->db->rowCount() > 0;
+        }
     }
 ?>

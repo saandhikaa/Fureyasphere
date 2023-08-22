4004f2ceab25312af7661bca4a90127dd1e6edd0
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-22 08:09:41 +0700

    add display sign in username

--- shared/views/templates/header.php
+++ shared/views/templates/header.php
@@ -13,3 +13,9 @@
 </head>
     
 <body>
+    <?php if (isset($_SESSION["sign-in"])): ?>
+        <strong><?= $_SESSION["sign-in"]["username"] ?></strong>
+        <br>
+        <br>
+        <br>
+    <?php endif ?>

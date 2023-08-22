0fc84e178b9f9efa8e14545f901494ecc32ea151
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-22 08:04:32 +0700

    add sign out button

--- shared/views/users/index.php
+++ shared/views/users/index.php
@@ -1,2 +1,10 @@
 <h1><?= "welcome " . $_SESSION["sign-in"]["username"] ?></h1>
 <a href="<?= BASEURL ?>/Clouds">Clouds service</a>
+
+<br>
+<br>
+<br>
+
+<form action="<?= BASEURL ?>/Users/login" method="post">
+    <input type="submit" name="submit" value="Sign Out">
+</form>

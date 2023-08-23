ca2c97c14a988e5e645de5521cb60f9937d344f1
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-23 08:57:50 +0700

    update views/upload

--- fuclouds/views/clouds/upload.php
+++ fuclouds/views/clouds/upload.php
@@ -1,20 +1,15 @@
 <div class="upload">
-    
-    <form action="<?= BASEURL ?>/Clouds/result" method="post" enctype="multipart/form-data">
+    <form action="" method="post" enctype="multipart/form-data">
         <ul id="staged-files"></ul>
         <div id="file-upload-container"></div>
-        <br>
         <button type="button" onclick="clickActiveInput()">Browse file</button>
         <br>
         <br>
-        <label for="codename">codename: </label>
+        <label for="codename">Give some keyword: </label>
+        <br>
         <input type="text" id="codename" name="codename" oninput="validateInput(this)" autocomplete="off" required>
         <br>
         <br>
-        <input type="submit" name="submit" value="Upload" id="up-submit" disabled>
+        <input type="submit" id="up-submit" name="submit" value="Upload" disabled>
     </form>
-    
-    <br>
-    <br>
-    <a href="<?= BASEURL ?>/Clouds/index">back home</a>
 </div>

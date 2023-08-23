3292e9433b2a4b12ae0cf954e4fed0a288fbd953
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-23 08:56:53 +0700

    update views/result

--- fuclouds/views/clouds/result.php
+++ fuclouds/views/clouds/result.php
@@ -1,13 +1,10 @@
 <div>
-    <?php if (isset($data["result"]["status"])): ?>
-        <p>
-            Your files uploaded successfully with codename <strong style="font-family: monospace;"><?= $data["result"]["status"]["codename"] ?> </strong>
-            and key <strong style="font-family: monospace;"><?php echo $data["result"]["status"]["key"]; ?></strong>
-        </p>
+    <?php if ($data["status"] === "uploaded"): ?>
+        <p>Your files uploaded successfully with keyword <strong style="font-family: monospace;"><?= $data["keyword"] ?> </strong></p>
     <?php endif ?>
     
     <ul>
-        <?php foreach ($data["result"]["files"] as $file): ?>
+        <?php foreach ($data["result"] as $file): ?>
             <li>
                 <div>
                     <p><?= $file["filename_"] ?></p>
@@ -15,6 +12,8 @@
                 </div>
                 <form action="<?= BASEURL ?>/Clouds/result" method="post">
                     <?php $filepath = $file["time_"] . "_" . $file["filename_"] ?>
+                    <input type="hidden" name="codename" value="<?= $file['codename_'] ?>">
+                    <input type="hidden" name="key" value="<?= $file['key_'] ?>">
                     <input type="hidden" name="filename" value="<?= $file['filename_'] ?>">
                     <input type="hidden" name="filepath" value="<?= $filepath ?>">
                     <input type="submit" name="submit" value="Download">
@@ -23,9 +22,11 @@
         <?php endforeach ?>
     </ul>
     
-    <?php if (count($data["result"]["files"]) > 1): ?>
+    <?php if (count($data["result"]) > 1): ?>
         <form action="<?= BASEURL ?>/Clouds/result" method="post">
-            <?php $filepath = $data["result"]["files"][0]["codename_"] . "_" . $data["result"]["files"][0]["key_"] . ".zip"?>
+            <?php $filepath = $data["result"][0]["codename_"] . "_" . $data["result"][0]["key_"] . ".zip" ?>
+            <input type="hidden" name="codename" value="<?= $file['codename_'] ?>">
+            <input type="hidden" name="key" value="<?= $file['key_'] ?>">
             <input type="hidden" name="filename" value="<?= $filepath ?>">
             <input type="hidden" name="filepath" value="<?= $filepath ?>">
             <input type="submit" name="submit" value="Download All as Zip">
@@ -34,9 +35,7 @@
         <br>
     <?php endif ?>
     
-    <?php if (empty($data["result"]["files"])): ?>
-        <p>wrong codename/key or the file is gone</p>
+    <?php if (empty($data["result"])): ?>
+        <p>invalid keyword or the file is gone</p>
     <?php endif ?>
-    
-    <a href="<?= BASEURL ?>/Clouds/index">back home</a>
 </div>

04aa626919807d96cc8921a57e5a5c2ec9e78907
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-23 10:42:19 +0700

    create header bar

--- shared/views/templates/header.php
+++ shared/views/templates/header.php
@@ -5,17 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $data["title"] ?></title>
-    <?php
-        if (isset($data["styles"])) {
-            echo $data["styles"];
-        }
-    ?>
+    
+    <?php if (isset($data["styles"])): ?>
+        <?= $data["styles"] ?>
+    <?php endif ?>
 </head>
     
 <body>
-    <?php if (isset($_SESSION["sign-in"])): ?>
-        <strong><?= $_SESSION["sign-in"]["username"] ?></strong>
-        <br>
-        <br>
-        <br>
-    <?php endif ?>
+    <div id="container">
+        <header>
+            <h1><?= $data["app"] ?></h1>
+            <div class="nav-button">
+                <?php if (isset($_SESSION["sign-in"])): ?>
+                    <strong><?= $_SESSION["sign-in"]["username"] ?></strong>
+                <?php endif ?>
+                <button type="button">menu</button>
+            </div>
+        </header>

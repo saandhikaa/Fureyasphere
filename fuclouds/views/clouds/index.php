c974518b72e690ab84bcc5fd89773a7e084fd154
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-23 08:57:24 +0700

    update views/index

--- fuclouds/views/clouds/index.php
+++ fuclouds/views/clouds/index.php
@@ -1,12 +1,13 @@
 <div>
-    <form action="<?= BASEURL ?>/Clouds/result" method="post" id="sr-form">
-        <label for="codename">codename: </label><br/>
-        <input type="text" id="codename" name="codename" oninput="validateInput(this)" required><br/>
-        <label for="key">key: </label><br/>
-        <input type="number" id="key" name="key" required><br/><br/>
-        <input type="submit" name="submit" value="Search" id="sr-submit" onclick="updateURL()">
+    <form action="" method="post">
+        <label for="keyword">Keyword: </label>
+        <br>
+        <input type="text" id="keyword" name="keyword" required>
+        <br>
+        <br>
+        <input type="submit" name="submit" value="Search">
     </form>
-    <br/>
-    <br/>
+    <br>
+    <br>
     <a href="<?= BASEURL ?>/Clouds/upload">Upload</a>
 </div>

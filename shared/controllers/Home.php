256f59e5377b27805ce61138100f38cfd90a1a57
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-26 07:11:37 +0700

    update construct

--- shared/controllers/Home.php
+++ shared/controllers/Home.php
@@ -1,6 +1,15 @@
 <?php
     class Home extends Controller {
+        private $app;
         private $data = [];
+        
+        public function __construct() {
+            $this->app = basename(dirname(__DIR__));
+            
+            $this->data["app"] = "Fureya Clouds Service";
+            $this->data["styles"] = '<link rel="stylesheet" href="' . BASEURL . '/' . $this->app . '/assets/css/style.css">';
+            $this->data["appScript"] = '<script src="' . BASEURL . '/' . $this->app . '/assets/js/main.js"></script>'; 
+        }
         
         public function index() {
             $this->data["title"] = "FUREYA CLOUDS SERVICE";

044c1f605cf6fd164fc5419626e3a4ea083d594f
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-08-31 09:56:28 +0700

    update semantic structure & attributes then remove unnecessary things

--- fuclouds-1/views/fuclouds/upload.php
+++ fuclouds-1/views/fuclouds/upload.php
@@ -1,18 +1,18 @@
-<div class="upload">
-    <form action="" method="post" enctype="multipart/form-data">
-        <ul id="staged-files"></ul>
-        <div id="file-upload-container"></div>
-        <button type="button" onclick="clickActiveInput()">Browse file</button>
-        <br>
-        <br>
-        <label for="codename">Give some keyword: </label>
-        <br>
-        <input type="text" id="codename" name="codename" oninput="validateInput(this)" autocomplete="off" required>
-        <br>
-        <br>
-        <input type="submit" id="up-submit" name="submit" value="Upload" disabled>
-    </form>
-    <br>
-    <br>
-    <a href="<?= BASEURL . '/' . $data['app'] ?>">Search</a>
-</div>
+<form action="" method="post" enctype="multipart/form-data">
+    <section id="file-input-container"></section>
+    <ul id="filtered-file"></ul>
+    
+    <section class="field">
+        <label for="input-file">Select a file:</label>
+        <button type="button" id="input-file">Browse file</button>
+    </section>
+    
+    <section class="field">
+        <label for="codename">Give a keyword:</label>
+        <input type="text" id="codename" name="codename" autocomplete="off" required>
+    </section>
+    
+    <input type="submit" id="up-submit" name="submit" value="Upload" disabled>
+</form>
+
+<a href="<?= BASEURL . '/' . $data['app'] ?>">Search</a>

364c7fb028b141f246cd919beb0739b3c0a744d5
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-09-02 13:55:27 +0700

    some fix

--- fuclouds-1/assets/js/app.js
+++ fuclouds-1/assets/js/app.js
@@ -1,84 +1,81 @@
-// Function to handle the 'change' event and access file information
-function handleFileChange(event) {
-    const selectedFiles = event.target.files;
-    if (selectedFiles.length > 0) {
-        for (let i = 0; i < selectedFiles.length; i++) {
-            createStagedFiles(selectedFiles[i].name);
-            submitButtonHandler();
-        }
-    }
-    event.target.classList.remove('active-input');
-    createInput();
-}
+Scanning.prototype.triggerInput = element => {
+    element.parentElement.parentElement.querySelector('.active-input').click();
+};
 
-function createStagedFiles(selectedFile) {
-    const stagedContainer = document.getElementById('staged-files');
-    const filelist = document.createElement('li');
-    filelist.classList.add('accepted-file');
+Scanning.prototype.cancelInput = element => {
+    element.closest('li').remove();
+    enablingUpload();
+};
+
+function enablingUpload() {
+    const upSubmit = document.querySelector('#fuclouds-upload #up-submit');
+    const filteredFile = document.querySelectorAll('#fuclouds-upload #filtered-file li');
     
-    const filepost = document.createElement('input');
-    filepost.type = 'hidden';
-    filepost.name = 'post[]';
-    filepost.value = selectedFile;
-    
-    const filename = document.createElement('p');
-    filename.textContent = selectedFile;
-    
-    const cancel = document.createElement('button');
-    cancel.type = 'button';
-    cancel.textContent = 'Cancel';
-    cancel.addEventListener('click', function() {
-        stagedContainer.removeChild(filelist);
-        submitButtonHandler();
-    });
-    
-    filelist.appendChild(filepost);
-    filelist.appendChild(filename);
-    filelist.appendChild(cancel);
-    stagedContainer.appendChild(filelist);
+    upSubmit.disabled = filteredFile.length === 0 ? true : false;
 }
 
 function createInput() {
-    const fileUploadContainer = document.getElementById('file-upload-container');
+    const fileUploadContainer = document.querySelector('#fuclouds-upload #file-input-container');
     
     // Create the input element with the specified attributes
     const inputFile = document.createElement('input');
     inputFile.type = 'file';
     inputFile.name = 'file[]';
-    inputFile.style.display = 'none';
-    inputFile.classList.add('file-input');
     inputFile.classList.add('active-input');
-    inputFile.addEventListener('change', handleFileChange);
     inputFile.multiple = true;
+    inputFile.addEventListener('change', handleFileChange);
      
     // Append the input element to the file input container div
     fileUploadContainer.appendChild(inputFile);
 }
 
-function clickActiveInput() {
-    var activeInput = document.querySelector('.active-input');
-    if (activeInput) {
-        activeInput.click();
+// Function to handle the 'change' event and access file information
+function handleFileChange(element) {
+    const selectedFiles = element.target.files;
+    if (selectedFiles.length > 0) {
+        for (let i = 0; i < selectedFiles.length; i++) {
+            createFilteredFile(selectedFiles[i].name, selectedFiles[i].size);
+            enablingUpload();
+        }
     }
+    element.target.removeAttribute('class');
+    createInput();
 }
 
-function submitButtonHandler() {
-    const upSubmit = document.getElementById('up-submit');
-    const filelist = document.querySelectorAll('#staged-files .accepted-file');
+function createFilteredFile(filename, filesize) {
+    const filteredFileContainer = document.querySelector('#fuclouds-upload #filtered-file');
+    const filteredFile = document.createElement('li');
+    const fileInfo = document.createElement('section');
     
-    if (filelist.length > 0) {
-        if (upSubmit.disabled) {
-            upSubmit.disabled = false;
-        }
-    } else {
-        if (!upSubmit.disabled) {
-            upSubmit.disabled = true;
-        }
-    }
+    const fileText = document.createElement('p');
+    fileText.textContent = filename;
+    
+    const fileSize = document.createElement('p');
+    fileSize.textContent = filesize;
+    
+    const fileInput = document.createElement('input');
+    fileInput.type = 'hidden';
+    fileInput.name = 'filteredFiles[]';
+    fileInput.value = filename;
+    
+    const cancel = document.createElement('button');
+    cancel.type = 'button';
+    cancel.classList.add('cancelInput');
+    cancel.textContent = 'Cancel';
+    
+    fileInfo.append(fileText, fileSize, fileInput);
+    filteredFile.append(fileInfo, cancel);
+    filteredFileContainer.appendChild(filteredFile);
 }
 
 
 
-function validateInput(inputElement) {
-    inputElement.value = inputElement.value.replace(/[^a-zA-Z0-9\-]/g, "");
+const searchKeyword = document.querySelector('#fuclouds-search #keyword');
+if (searchKeyword) {
+    searchKeyword.addEventListener('input', element => element.target.value = element.target.value.replace(/[^a-zA-Z0-9/-]/g, ""));
+}
+
+const uploadCodename = document.querySelector('#fuclouds-upload #codename');
+if (uploadCodename) {
+    uploadCodename.addEventListener('input', element => element.target.value = element.target.value.replace(/[^a-zA-Z0-9-]/g, ""));
 }

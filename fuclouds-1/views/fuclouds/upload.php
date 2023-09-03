f60bf0ef6f34498b412de12db1b2a6474473e6f8
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-09-03 10:50:05 +0700

    update attributes

--- fuclouds-1/views/fuclouds/upload.php
+++ fuclouds-1/views/fuclouds/upload.php
@@ -1,18 +1,20 @@
-<form action="" method="post" enctype="multipart/form-data">
-    <section id="file-input-container"></section>
-    <ul id="filtered-file"></ul>
+<main id="fuclouds-upload">
+    <form action="" method="post" enctype="multipart/form-data">
+        <section id="file-input-container"></section>
+        <ul id="filtered-file"></ul>
+        
+        <section class="field">
+            <label for="trigger-input">Select a file:</label>
+            <button type="button" id="trigger-input" class="triggerInput">Browse file</button>
+        </section>
+        
+        <section class="field">
+            <label for="codename">Give a keyword:</label>
+            <input type="text" id="codename" name="codename" autocomplete="off" required>
+        </section>
+        
+        <input type="submit" id="up-submit" name="submit" value="Upload" disabled>
+    </form>
     
-    <section class="field">
-        <label for="input-file">Select a file:</label>
-        <button type="button" id="input-file">Browse file</button>
-    </section>
-    
-    <section class="field">
-        <label for="codename">Give a keyword:</label>
-        <input type="text" id="codename" name="codename" autocomplete="off" required>
-    </section>
-    
-    <input type="submit" id="up-submit" name="submit" value="Upload" disabled>
-</form>
-
-<a href="<?= BASEURL . '/' . $data['app'] ?>">Search</a>
+    <a href="<?= BASEURL . '/' . $data['app'] ?>">Search</a>
+</main>

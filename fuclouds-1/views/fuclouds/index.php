a6987ce5f1a17d3038349b22dc0eef0eda54c98b
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-09-03 10:48:33 +0700

    add main element and remove br element

--- fuclouds-1/views/fuclouds/index.php
+++ fuclouds-1/views/fuclouds/index.php
@@ -1,13 +1,9 @@
-<div>
+<main id="fuclouds-search">
     <form action="" method="post">
         <label for="keyword">Keyword: </label>
-        <br>
         <input type="text" id="keyword" name="keyword" required>
-        <br>
-        <br>
         <input type="submit" name="submit" value="Search">
     </form>
-    <br>
-    <br>
+    
     <a href="<?= BASEURL . '/' . $data['app']?>/upload">Upload</a>
-</div>
+</main>

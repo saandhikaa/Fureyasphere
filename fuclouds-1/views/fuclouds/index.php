4ea1c35dea4f2f06949b78d57ad2759f17710059
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-09-09 22:38:42 +0700

    update form: add headline & fix sttucture

--- fuclouds-1/views/fuclouds/index.php
+++ fuclouds-1/views/fuclouds/index.php
@@ -1,8 +1,14 @@
 <main id="fuclouds-search">
     <form action="" method="post">
+        <h2>Search for Files</h2>
+        <p>Use the search bar below to find a file you need.</p>
+        
         <label for="keyword">Keyword: </label>
-        <input type="text" id="keyword" name="keyword" required>
-        <input type="submit" name="submit" value="Search">
+        <section>
+            <input type="text" id="keyword" name="keyword" required>
+            <input type="button" class="clear" value="Clear">
+            <input type="submit" name="submit" value="Search">
+        </section>
     </form>
     
     <a href="<?= BASEURL . '/' . $data['app']?>/upload">Upload</a>

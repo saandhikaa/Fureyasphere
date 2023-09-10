a0477665b3be10da0641e79e7bca838fa67c01ad
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-09-10 11:03:06 +0700

    update structure: search form: remove clear button & add field group container

--- fuclouds-1/views/fuclouds/index.php
+++ fuclouds-1/views/fuclouds/index.php
@@ -3,11 +3,12 @@
         <h2>Search for Files</h2>
         <p>Use the search bar below to find a file you need.</p>
         
-        <label for="keyword">Keyword: </label>
-        <section>
-            <input type="text" id="keyword" name="keyword" required>
-            <input type="button" class="clear" value="Clear">
-            <input type="submit" name="submit" value="Search">
+        <section class="group-input">
+            <label for="keyword">Keyword: </label>
+            <div class="field">
+                <input type="text" id="keyword" name="keyword" autocomplete="off" placeholder="Input keyword.. " required>
+                <input type="submit" name="submit" value="Search">
+            </div>
         </section>
     </form>
     

8dad8397905c8810ce6b9dc3352ec2ef733bc86e
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-09-10 11:10:05 +0700

    update structure: upload form

--- fuclouds-1/views/fuclouds/upload.php
+++ fuclouds-1/views/fuclouds/upload.php
@@ -3,19 +3,20 @@
         <section id="file-input-container"></section>
         <ul id="filtered-file"></ul>
         
-        <section class="field">
+        <?= isset($_SESSION["sign-in"]["username"]) ? '<input type="hidden" name="owner" value="' . $_SESSION["sign-in"]["username"] . '">' : '' ?>
+        
+        <section class="browse-file">
             <label for="trigger-input">Select a file:</label>
             <button type="button" id="trigger-input" class="triggerInput">Browse file</button>
         </section>
         
-        <section class="field">
+        <section class="group-input">
             <label for="codename">Give a keyword:</label>
-            <input type="text" id="codename" name="codename" <?= isset($_SESSION["sign-in"]["username"]) ? 'value="' . $_SESSION["sign-in"]["username"] . '"' : '' ?> autocomplete="off" required>
+            <div class="field">
+                <input type="text" id="codename" name="codename" <?= isset($_SESSION["sign-in"]["username"]) ? 'value="' . $_SESSION["sign-in"]["username"] . '"' : '' ?> placeholder="Input keyword..." autocomplete="off" required>
+                <input type="submit" id="up-submit" name="submit" value="Upload" disabled>
+            </div>
         </section>
-        
-        <?= isset($_SESSION["sign-in"]["username"]) ? '<input type="hidden" name="owner" value="' . $_SESSION["sign-in"]["username"] . '">' : '' ?>
-        
-        <input type="submit" id="up-submit" name="submit" value="Upload" disabled>
     </form>
     
     <a href="<?= BASEURL . '/' . $data['app'] ?>">Search</a>

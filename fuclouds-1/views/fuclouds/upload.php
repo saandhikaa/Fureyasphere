680cfea9102e00d7dd0eed74b7e3399a5b03942c
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-09-11 22:14:32 +0700

    add hidden thumbnail source text & refactoring

--- fuclouds-1/views/fuclouds/upload.php
+++ fuclouds-1/views/fuclouds/upload.php
@@ -1,21 +1,26 @@
 <main id="fuclouds-upload">
+    <p class="image-path"><?= BASEURL . '/' . basename(dirname(dirname(dirname(__FILE__)))) . '/assets/images/file-type-icon/' ?></p>
+    
     <form action="" method="post" enctype="multipart/form-data">
-        <section id="file-input-container"></section>
-        <ul id="filtered-file"></ul>
-        
-        <?= isset($_SESSION["sign-in"]["username"]) ? '<input type="hidden" name="owner" value="' . $_SESSION["sign-in"]["username"] . '">' : '' ?>
-        
-        <section class="browse-file">
-            <label for="trigger-input">Select a file:</label>
-            <button type="button" id="trigger-input" class="triggerInput">Browse file</button>
+        <section id="file-input-container">
+            <ul id="filtered-file"></ul>
         </section>
         
-        <section class="group-input">
-            <label for="codename">Give a keyword:</label>
-            <div class="field">
-                <input type="text" id="codename" name="codename" <?= isset($_SESSION["sign-in"]["username"]) ? 'value="' . $_SESSION["sign-in"]["username"] . '"' : '' ?> placeholder="Input keyword..." autocomplete="off" required>
-                <input type="submit" id="up-submit" name="submit" value="Upload" disabled>
-            </div>
+        <section class="input-form">
+            <?= isset($_SESSION["sign-in"]["username"]) ? '<input type="hidden" name="owner" value="' . $_SESSION["sign-in"]["username"] . '">' : '' ?>
+            
+            <section class="browse-file">
+                <label for="trigger-input">Select a file:</label>
+                <button type="button" id="trigger-input" class="triggerInput">Browse file</button>
+            </section>
+            
+            <section class="group-input">
+                <label for="codename">Give a keyword:</label>
+                <div class="field">
+                    <input type="text" id="codename" name="codename" <?= isset($_SESSION["sign-in"]["username"]) ? 'value="' . $_SESSION["sign-in"]["username"] . '"' : '' ?> placeholder="Input keyword..." autocomplete="off" required>
+                    <input type="submit" id="up-submit" name="submit" value="Upload" disabled>
+                </div>
+            </section>
         </section>
     </form>
     

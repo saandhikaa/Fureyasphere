b62879ab6d4e7b9e874587fbe047bb684a9a675d
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-09-13 07:42:10 +0700

    create resources page: icon from flaticon

---
+++ fuclouds-1/views/fuclouds/resources.php
@@ -0,0 +1,39 @@
+<main>
+    <section>
+        <h2>Flaticon</h2>
+        <ul>
+            <li><a href="https://www.flaticon.com/free-icons/aac" title="aac icons">Aac icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/avi" title="avi icons">Avi icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/bmp" title="bmp icons">Bmp icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/css" title="css icons">Css icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/corel" title="corel icons">Corel icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/csv" title="csv icons">Csv icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/document" title="document icons">Document icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/eps" title="eps icons">Eps icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/files-and-folders" title="files and folders icons">Files and folders icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/html" title="html icons">Html icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/java" title="java icons">Java icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/javascript" title="javascript icons">Javascript icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/jpg" title="jpg icons">Jpg icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/mkv" title="mkv icons">Mkv icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/mov" title="mov icons">Mov icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/mp3" title="mp3 icons">Mp3 icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/mp4" title="mp4 icons">Mp4 icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/mpeg" title="mpeg icons">Mpeg icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/pdf" title="pdf icons">Pdf icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/png" title="png icons">Png icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/powerpoint" title="powerpoint icons">Powerpoint icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/rar" title="rar icons">Rar icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/rss" title="rss icons">Rss icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/sys" title="sys icons">Sys icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/sys" title="sys icons">Sys icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/structure-plan" title="structure plan icons">Structure plan icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/winzip" title="winzip icons">Winzip icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/xml" title="xml icons">Xml icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/xls" title="xls icons">Xls icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/xlsx" title="xlsx icons">Xlsx icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/iso" title="iso icons">Iso icons created by Graphix Dxinerz - Flaticon</a></li>
+            <li><a href="https://www.flaticon.com/free-icons/autocad" title="autocad icons">Autocad icons created by Graphix Dxinerz - Flaticon</a></li>
+        </ul> 
+    </section>
+</main>

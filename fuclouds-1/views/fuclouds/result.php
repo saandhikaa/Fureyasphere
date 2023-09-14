8bb92f89402a4a7fa9696543cf132bf3ebdb1719
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-09-14 14:42:42 +0700

    fix structure and add title & navigation

--- fuclouds-1/views/fuclouds/result.php
+++ fuclouds-1/views/fuclouds/result.php
@@ -1,14 +1,22 @@
 <main id="fuclouds-result">
+    <section class="header">
+        <h2>Search Results</h2>
+        <p>Here are the files that match your search keyword:</p>
+    </section>
+    
     <?php if ($data["action"] === "uploaded"): ?>
         <p class="status"><?= FileHandler::uploadSuccess($data["keyword"]) ?></p>
     <?php endif ?>
     
-    <ul>
+    <ul class="file-list">
         <?php foreach ($data["result"] as $file): ?>
             <li>
-                <section>
-                    <p class="fileName"><?= $file["filename_"] ?></p>
-                    <p class="fileSize"><?= FileHandler::formatBytes($file["filesize_"]) ?></p>
+                <section class="file-info">
+                    <img src="<?= BASEURL . '/' . basename(dirname(dirname(dirname(__FILE__)))) . '/assets/images/file-type-icon/' . pathinfo($file["filename_"], PATHINFO_EXTENSION) . '.png' ?>" alt="">
+                    <section class="file-dec">
+                        <p class="file-name"><?= $file["filename_"] ?></p>
+                        <p class="file-size"><?= FileHandler::formatBytes($file["filesize_"]) ?></p>
+                    </section>
                 </section>
                 <form action="" method="post">
                     <?php $filepath = $file["time_"] . "_" . $file["filename_"] ?>
@@ -16,20 +24,20 @@
                     <input type="hidden" name="key" value="<?= $file['key_'] ?>">
                     <input type="hidden" name="filename" value="<?= $file['filename_'] ?>">
                     <input type="hidden" name="filepath" value="<?= $filepath ?>">
-                    <input type="submit" name="submit" value="Download">
+                    <button type="submit" name="submit" class="file-action"><?php readfile(__DIR__ . '/../../assets/images/icons/download_FILL0_wght400_GRAD0_opsz24.svg') ?></button>
                 </form>
             </li>
         <?php endforeach ?>
     </ul>
     
     <?php if (count($data["result"]) > 1): ?>
-        <form action="" method="post">
+        <form action="" method="post" class="download-all">
             <?php $filepath = $data["result"][0]["codename_"] . "_" . $data["result"][0]["key_"] . ".zip" ?>
             <input type="hidden" name="codename" value="<?= $file['codename_'] ?>">
             <input type="hidden" name="key" value="<?= $file['key_'] ?>">
             <input type="hidden" name="filename" value="<?= $filepath ?>">
             <input type="hidden" name="filepath" value="<?= $filepath ?>">
-            <input type="submit" name="submit" value="Download All as Zip">
+            <button type="submit" name="submit" id="download-all">Download all as Zip <?php readfile(__DIR__ . '/../../assets/images/icons/download_FILL0_wght400_GRAD0_opsz24.svg') ?></button>
         </form>
     <?php endif ?>
     

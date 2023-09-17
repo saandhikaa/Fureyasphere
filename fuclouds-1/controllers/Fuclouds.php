312786150b528b01b2f7cf15702e78392e822778
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-09-17 14:08:15 +0700

    add data image-path

--- fuclouds-1/controllers/Fuclouds.php
+++ fuclouds-1/controllers/Fuclouds.php
@@ -13,6 +13,7 @@
             $this->data["app"] = $this->class;
             $this->data["styles"] = '<link rel="stylesheet" href="' . BASEURL . '/' . $this->app . '/assets/css/app.css">' . PHP_EOL;
             $this->data["appScript"] = '<script src="' . BASEURL . '/' . $this->app . '/assets/js/app.js"></script>' . PHP_EOL;
+            $this->data["image-path"] = '<p class="image-path">' . BASEURL . '/' . $this->app . '/assets/images/</p>' . PHP_EOL;
         }
         
         public function index() {

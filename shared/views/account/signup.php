f5cb7aa9081b8a81674402f19dee262bcb2ed828
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-09-20 11:03:26 +0700

    adjust element & attributes

--- shared/views/account/signup.php
+++ shared/views/account/signup.php
@@ -5,7 +5,7 @@
         <section class="group-input">
             <label for="username">Username:</label>
             <div class="field">
-                <input type="text" id="username" name="username" autocomplete="off" required>
+                <input type="text" id="username" name="username" autocomplete="off" placeholder="give some unique username" required>
             </div>
             <ul>
                 <li class="validation username-availability"><?php readfile(__DIR__ . '/../../assets/images/icons/check_circle_FILL0_wght400_GRAD0_opsz24.svg') ?>Username available</li>
@@ -17,7 +17,7 @@
         <section class="group-input">
             <label for="password">Password:</label>
             <div class="field">
-                <input type="password" id="password" name="password" autocomplete="off" required>
+                <input type="password" id="password" name="password" autocomplete="off" placeholder="give a password" required>
                 <button type="button" class="passwordVisibility"><?php readfile(__DIR__ . '/../../assets/images/icons/visibility_off_FILL0_wght400_GRAD0_opsz24.svg') ?></button>
             </div>
         </section>
@@ -25,7 +25,7 @@
         <section class="group-input">
             <label for="confirm-password">Confirm Password:</label>
             <div class="field">
-                <input type="password" id="confirm-password" name="confirm-password" autocomplete="off" required>
+                <input type="password" id="confirm-password" name="confirm-password" autocomplete="off" placeholder="re-type your password" required>
                 <button type="button" class="passwordVisibility"><?php readfile(__DIR__ . '/../../assets/images/icons/visibility_off_FILL0_wght400_GRAD0_opsz24.svg') ?></button>
             </div>
             <span class="validation passwords-match"><?php readfile(__DIR__ . '/../../assets/images/icons/check_circle_FILL0_wght400_GRAD0_opsz24.svg') ?>Passwords match</span>
@@ -33,11 +33,11 @@
         
         <section class="agreement">
             <input type="checkbox" id="agreement" name="agreement" required>
-            <label for="agreement">I agree to the <a href="">Privacy</a> and <a href="">Terms</a></label><br>
+            <label for="agreement">I agree to the <a class="brandeis" href="">Privacy</a> and <a class="brandeis" href="">Terms</a></label>
         </section>
         
-        <input type="submit" name="submit" id="su-submit" value="Sign Up" disabled>
+        <input type="submit" name="submit" id="su-submit" class="action-button" value="Sign Up" disabled>
     </form>
     
-    <p>Already have an account?  <a href="<?= BASEURL . '/' . $data['app'] ?>/signin">Sign In</a></p>
+    <section class="go">Already have an account?  <a class="brandeis" href="<?= BASEURL . '/' . $data['app'] ?>/signin">Sign In</a></section>
 </main>

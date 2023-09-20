05ebc701c9db3cea5c96de113659129e2981a860
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-09-20 12:48:28 +0700

    adjust element & attributes

--- shared/views/account/signin.php
+++ shared/views/account/signin.php
@@ -5,26 +5,22 @@
         <section class="group-input">
             <label for="username">Username:</label>
             <div class="field">
-                <input type="text" id="username" name="username" autocomplete="off" required>
+                <input type="text" id="username" name="username" autocomplete="off" placeholder="input your username" required>
             </div>
         </section>
         
         <section class="group-input">
             <label for="password">Password:</label>
             <div class="field">
-                <input type="password" id="password" name="password" autocomplete="off" required>
+                <input type="password" id="password" name="password" autocomplete="off" placeholder="input your password" required>
                 <button type="button" class="passwordVisibility"><?php readfile(__DIR__ . '/../../assets/images/icons/visibility_off_FILL0_wght400_GRAD0_opsz24.svg') ?></button>
             </div>
         </section>
         
-        <span><?= isset($data["sign-in-failed"]) ? $data["sign-in-failed"] : "" ?></span>
+        <span class="sign-in-failed"><?= isset($data["sign-in-failed"]) ? "*" . $data["sign-in-failed"] : "" ?></span>
         
-        <br>
+        <input type="submit" name="submit" id="si-submit" class="action-button" value="Sign In">
         
-        <input type="submit" name="submit" id="si-submit" value="Sign In">
-        
-        <br>
-        
-        <p>Don't have an account?  <a href="<?= BASEURL . '/' . $data['app'] ?>/signup">Sign Up</a></p>
+        <section class="go">Don't have an account?<a class="brandeis" href="<?= BASEURL . '/' . $data['app'] ?>/signup">Sign Up</a></section>
     </form>
 </main>

0f474bc6df753e2929ce42747017439c65621227
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-09-22 22:05:53 +0700

    some fix

--- shared/views/templates/header.php
+++ shared/views/templates/header.php
@@ -26,7 +26,7 @@
         <nav class="navigation">
             <header>
                 <section>
-                    <h1><?= isset($_SESSION["sign-in"]["username"]) ? $_SESSION["sign-in"]["username"] : "hello" ?></h1>
+                    <?= isset($_SESSION["sign-in"]["username"]) ? '<h1>Hi, ' . $_SESSION["sign-in"]["username"] . '</h1>' : '<h1 class="nav-greeting"></h1>' ?>
                 </section>
                 <button type="button" class="closeNav nav-button"><?php readfile(__DIR__ . '/../../assets/images/icons/close_FILL0_wght400_GRAD0_opsz24.svg') ?></button>
                 <span class="separator bottom"></span>
@@ -34,8 +34,8 @@
             
             <section class="list">
                 <ul class="main-list row">
-                    <li><a href="<?= BASEURL ?>/home"><?php readfile(__DIR__ . '/../../assets/images/icons/home_FILL0_wght400_GRAD0_opsz24.svg') ?>Home</a></li>
-                    <li><a href="<?= BASEURL ?>/account"><?php readfile(__DIR__ . '/../../assets/images/icons/person_FILL0_wght400_GRAD0_opsz24.svg') ?>Account</a></li>
+                    <li><a href="<?= BASEURL ?>/home"><?php readfile(__DIR__ . '/../../assets/images/icons/home_FILL0_wght400_GRAD0_opsz24.svg') ?><span>Home</span></a></li>
+                    <li><a href="<?= BASEURL ?>/account"><?php readfile(__DIR__ . '/../../assets/images/icons/person_FILL0_wght400_GRAD0_opsz24.svg') ?><span>Account</span></a></li>
                 </ul>
                 <span class="separator"></span>
                 <ul class="app-list row">
@@ -51,8 +51,8 @@
             <footer>
                 <span class="separator top"></span>
                 <ul class="row">
-                    <li><a href="https://github.com/saandhikaa" target="_blank">Follow me on GitHub</a></li>
-                    <li><a href="">Leave a comment</a></li>
+                    <li><a href="">Leave a Comment</a></li>
+                    <li><a href="https://github.com/saandhikaa/fureya-clouds-service/issues" target="_blank">Create an Issue on GitHub</a></li>
                 </ul>
                 <p class="copyright"><span></span> <?= ME ?>.</p>
                 <ul class="footer-list">

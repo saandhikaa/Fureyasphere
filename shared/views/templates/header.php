2e8aaa0e820a9553c2d06590c085b10ada0a9878
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-09-24 23:30:56 +0700

    update layout & attribute

--- shared/views/templates/header.php
+++ shared/views/templates/header.php
@@ -15,7 +15,7 @@
 </head>
 
 <body>
-    <header id="page-header">
+    <header id="page-header" class="mobile">
         <h1><?= ucfirst($data["app"]) ?></h1>
         <button type="button" class="openNav nav-button"><?= isset($_SESSION["sign-in"]["username"]) ? '<span>' . strtoupper($_SESSION["sign-in"]["username"][0]) . '</span>' : (function() { readfile(__DIR__ . '/../../assets/images/icons/menu_FILL0_wght400_GRAD0_opsz24.svg'); })() ?></button>
     </header>
@@ -24,7 +24,7 @@
     
     <div class="navigation-container closeNav">
         <nav class="navigation">
-            <header>
+            <header class="mobile">
                 <section>
                     <?= isset($_SESSION["sign-in"]["username"]) ? '<h1>Hi, ' . $_SESSION["sign-in"]["username"] . '</h1>' : '<h1 class="nav-greeting"></h1>' ?>
                 </section>
@@ -32,14 +32,15 @@
                 <span class="separator bottom"></span>
             </header>
             
+            <h1 class="desktop"><?= ucfirst($data["app"]) ?></h1>
+            
             <section class="list">
                 <ul class="main-list row">
                     <li><a href="<?= BASEURL ?>/home"><?php readfile(__DIR__ . '/../../assets/images/icons/home_FILL0_wght400_GRAD0_opsz24.svg') ?><span>Home</span></a></li>
-                    <li><a href="<?= BASEURL ?>/account"><?php readfile(__DIR__ . '/../../assets/images/icons/person_FILL0_wght400_GRAD0_opsz24.svg') ?><span>Account</span></a></li>
+                    <li class="mobile"><a href="<?= BASEURL ?>/account"><?php readfile(__DIR__ . '/../../assets/images/icons/person_FILL0_wght400_GRAD0_opsz24.svg') ?><span>Account</span></a></li>
                 </ul>
-                <span class="separator"></span>
                 <ul class="app-list row">
-                    <h6>Services</h6>
+                    <h6 class="mobile">Services</h6>
                     <?php foreach (App::getAppListNavigation() as $appControllers): ?>
                         <?php foreach ($appControllers as $controller): ?>
                             <li><a href="<?= BASEURL .  '/' . strtolower($controller) ?>"><?= $controller ?></a></li>
@@ -50,17 +51,26 @@
             
             <footer>
                 <span class="separator top"></span>
-                <ul class="row">
-                    <li><a href="">Leave a Comment</a></li>
-                    <li><a href="https://github.com/saandhikaa/fureya-clouds-service/issues" target="_blank">Create an Issue on GitHub</a></li>
+                <button class="feedbackList desktop">Feedback <?php readfile(__DIR__ . '/../../assets/images/icons/expand_more_FILL0_wght400_GRAD0_opsz24.svg') ?></button>
+                <ul class="row feedback-list">
+                    <li><a href=""><span class="mobile">Leave a </span>Comment</a></li>
+                    <li><a href="https://github.com/saandhikaa/fureya-clouds-service/issues" target="_blank"><span class="mobile">Create an Issue on GitHub</span><span class="desktop">GitHub Issue</span></a></li>
                 </ul>
-                <p class="copyright"><span></span> <?= ME ?>.</p>
-                <ul class="footer-list">
+                <p class="copyright mobile"><span></span> <?= ME ?>.</p>
+                <ul class="footer-list mobile">
                     <li><a href="">About</a></li>
                     <li><a href="">Resources</a></li>
                     <li><a href="">Terms</a></li>
                     <li><a href="">Privacy</a></li>
                 </ul>
             </footer>
+            
+            <section class="account-info desktop">
+                <?php if(isset($_SESSION["sign-in"]["username"])): ?>
+                    <a href="<?= BASEURL ?>/account"><?= $_SESSION["sign-in"]["username"] ?></a>
+                <?php else: ?>
+                    <a href="<?= BASEURL ?>/account/signin">Sign in</a>
+                <?php endif ?>
+            </section>
         </nav>
     </div>

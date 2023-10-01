9ef467fca96faf8bd7dc17e7953cbf1cbbd2f486
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-01 17:12:36 +0700

    create controller method: about

--- shared/controllers/Home.php
+++ shared/controllers/Home.php
@@ -8,15 +8,26 @@
             $this->app = basename(dirname(__DIR__));
             
             $this->data["mainApp"] = $this->app;
-            $this->data["app"] = "Fureya Clouds Service";
+            $this->data["app"] = $this->class;
             $this->data["issue"] = "https://github.com/saandhikaa/fureya-clouds-service/issues";
         }
         
         public function index() {
-            $this->data["title"] = strtoupper($this->data["app"]);
+            $this->data["title"] = "FUREYA CLOUDS SERVICE";
             
             $this->view($this->app, "templates/header", $this->data);
             $this->view($this->app, "$this->class/index");
+            $this->view($this->app, "templates/footer", $this->data);
+        }
+        
+        public function about() {
+            $this->data["title"] = "FUREYA CLOUDS SERVICE" ;
+            $this->data["appScript"] = '<script type="text/javascript">loadReadme();</script>' . PHP_EOL;
+            
+            $content = '<section class="readme shared"></section>';
+            
+            $this->view($this->app, "templates/header", $this->data);
+            echo $content;
             $this->view($this->app, "templates/footer", $this->data);
         }
     }

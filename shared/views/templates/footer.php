9aae44b7d5113f690ea10e32da26b2db348af33f
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-02 08:11:39 +0700

    include marked library

--- shared/views/templates/footer.php
+++ shared/views/templates/footer.php
@@ -1,3 +1,4 @@
+    <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
     <script src="<?= BASEURL . '/' . $data["mainApp"] ?>/assets/js/main.js"></script>
     <?= isset($data["appScript"]) ? $data["appScript"] : "" ?>
 </body>

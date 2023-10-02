e0c3f60e8fa03a789f3b80a7faac97b3770541be
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-02 21:19:32 +0700

    update controller method: about: load all app's README.md

--- shared/controllers/Home.php
+++ shared/controllers/Home.php
@@ -8,26 +8,27 @@
             $this->appDir = basename(dirname(__DIR__));
             
             $this->data["mainAppDir"] = $this->appDir;
+            $this->data["title"] = "FUREYA CLOUDS SERVICE";
             $this->data["class"] = $this->class;
             $this->data["issue"] = "https://github.com/saandhikaa/fureya-clouds-service/issues";
         }
         
         public function index() {
-            $this->data["title"] = "FUREYA CLOUDS SERVICE";
-            
             $this->view($this->appDir, "templates/header", $this->data);
             $this->view($this->appDir, "$this->class/index");
             $this->view($this->appDir, "templates/footer", $this->data);
         }
         
         public function about() {
-            $this->data["title"] = "FUREYA CLOUDS SERVICE" ;
             $this->data["appScript"] = '<script type="text/javascript">loadReadme();</script>' . PHP_EOL;
             
-            $content = '<section class="readme shared"></section>';
-            
             $this->view($this->appDir, "templates/header", $this->data);
-            echo $content;
+            echo '<main id="home-about">' . PHP_EOL;
+            echo '<section class="readme shared"></section>' . PHP_EOL;
+            foreach (App::getAppList() as $appDir => $class) {
+                echo '<section class="readme ' . $appDir . '"></section>' . PHP_EOL;
+            }
+            echo '</main>' . PHP_EOL;
             $this->view($this->appDir, "templates/footer", $this->data);
         }
     }

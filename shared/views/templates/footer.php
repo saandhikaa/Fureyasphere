179e55f6f72fc837ab0774813ba43a3ee486e91b
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-03 08:33:53 +0700

    add library: Eruda for mobile

--- shared/views/templates/footer.php
+++ shared/views/templates/footer.php
@@ -1,3 +1,7 @@
+    <!-- Enable console: Eruda for mobile -->
+    <script src="//cdn.jsdelivr.net/npm/eruda"></script>
+    <script>eruda.init();</script>
+    
     <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
     <script src="<?= BASEURL . '/' . $data["mainAppDir"] ?>/assets/js/main.js"></script>
     <?= isset($data["appScript"]) ? $data["appScript"] : "" ?>

f6841a468711c8069c69fced56ba793403e29b6e
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-03 12:12:31 +0700

    add database existence check

--- index.php
+++ index.php
@@ -3,5 +3,11 @@
     
     if (!session_id()) session_start();
     
+    if (!(new Database())->databaseExists(DB_NAME)) {
+        echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
+        echo '<h2>Database "' . DB_NAME . '" not found, please create it.</h2>';
+        die;
+    }
+    
     new App;
 ?>

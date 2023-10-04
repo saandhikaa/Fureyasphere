fc3b492b0b5ec5279933b3688018285a153aeac8
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-04 12:47:29 +0700

    refactoring

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
     require_once __DIR__ . "/shared/init.php";
     
-    if (!session_id()) session_start();
-    
-    if (!(new Database())->databaseExists(DB_NAME)) {
-        echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
-        echo '<h2>Database "' . DB_NAME . '" not found, please create it.</h2>';
-        die;
+    if (!session_id()) {
+        session_start();
     }
     
+    new Database;
     new App;
 ?>

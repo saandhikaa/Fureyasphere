3786b52cdf7a4d53eb6e1ef1e1c41d03e5421a7c
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-05 16:50:45 +0700

    create page: privacy policy

---
+++ shared/views/home/privacy-policy.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Privacy Policy</title>
+    
+    <style>
+        body {
+            padding: 20px 20px 100px;
+        }
+        
+        h1 {
+            font-size: 2rem;
+        }
+        
+        h2 {
+            margin-top: 50px;
+            font-size: 1.2rem;
+        }
+        
+        li {
+            margin-top: 10px;
+        }
+        
+        a, a:visited {
+            color: #2B73E6;
+        }
+    </style>
+</head>
+<body>
+    <h1>Privacy Policy</h1>
+    <p><strong>Last Updated:</strong> September 05, 2023</p>
+    <p>This Privacy Policy outlines how <?= $data["web-application"] ?> ("we," "our," or "us") collects, uses, and protects your personal information when you access or use our website and services. By using <?= $data["web-application"] ?>, you consent to the practices described in this Privacy Policy.</p>
+    
+    <h2>Information We Collect</h2>
+    <p>We may collect the following types of information when you use <?= $data["web-application"] ?>:</p>
+    <ul>
+        <li>Personal information, such as your name and email address, when you voluntarily provide it to us.</li>
+        <li>Usage data, including information about your interactions with the web app, such as the files you upload and download.</li>
+        <li>Log data, which may include your IP address, browser type, and other technical details.</li>
+    </ul>
+    
+    <h2>How We Use Your Information</h2>
+    <p>We use the information we collect for the following purposes:</p>
+    <ul>
+        <li>To provide and maintain <?= $data["web-application"] ?> and its services.</li>
+        <li>To improve and personalize your experience with <?= $data["web-application"] ?>.</li>
+        <li>To analyze usage patterns and trends.</li>
+        <li>To respond to your requests and inquiries.</li>
+        <li>To enforce our Terms of Service and protect the security of our users.</li>
+    </ul>
+    
+    <h2>Sharing Your Information</h2>
+    <p>We do not share your personal information with third parties, except in the following circumstances:</p>
+    <ul>
+        <li>With your consent.</li>
+        <li>When required by law or to protect our rights and safety.</li>
+    </ul>
+    
+    <h2>Security</h2>
+    <p>We take reasonable measures to protect your personal information. However, please be aware that no method of transmission over the internet or electronic storage is entirely secure, and we cannot guarantee absolute security.</p>
+    
+    <h2>Changes to this Privacy Policy</h2>
+    <p>We may update this Privacy Policy from time to time to reflect changes in our practices or for other operational, legal, or regulatory reasons. The revised Privacy Policy will be posted on <?= $data["web-application"] ?>, and we encourage you to review it periodically.</p>
+    
+    <h2>Contact Us</h2>
+    <p>If you have any questions or concerns about this Privacy Policy or our data practices, please contact us at <a href="mailto:snowhite.power@outlook.com?subject=Fureya:%20privacy">snowhite.power@outlook.com</a>.</p>
+</body>
+</html>

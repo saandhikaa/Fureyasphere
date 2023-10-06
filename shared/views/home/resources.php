6ef8bb5377bc9fd514d29483988583843ba6b191
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-06 22:16:09 +0700

    create page: resources

---
+++ shared/views/home/resources.php
@@ -0,0 +1,13 @@
+<ul class="google-fonts fonts">
+    <li><a href="https://fonts.google.com/share?selection.family=Ubuntu:wght@300;400;500;700" target="_blank">Font family: Ubuntu</a></li>
+</ul>
+
+<ul class="google-fonts icons">
+    <li><a href="https://fonts.google.com/icons?selected=Material%20Symbols%20Outlined%3Acheck_circle%3AFILL%400%3Bwght%40400%3BGRAD%400%3Bopsz%4024" target="_blank">Icon: Check Circle</a></li>
+    <li><a href="https://fonts.google.com/icons?selected=Material%20Symbols%20Outlined%3Aclose%3AFILL%400%3Bwght%40400%3BGRAD%400%3Bopsz%4024" target="_blank">Icon: Close</a></li>
+    <li><a href="https://fonts.google.com/icons?selected=Material%20Symbols%20Outlined%3Ahome%3AFILL%400%3Bwght%40400%3BGRAD%400%3Bopsz%4024" target="_blank">Icon: Home</a></li>
+    <li><a href="https://fonts.google.com/icons?selected=Material%20Symbols%20Outlined%3Amenu%3AFILL%400%3Bwght%40400%3BGRAD%400%3Bopsz%4024" target="_blank">Icon: Menu</a></li>
+    <li><a href="https://fonts.google.com/icons?selected=Material%20Symbols%20Outlined%3Aperson%3AFILL%400%3Bwght%40400%3BGRAD%400%3Bopsz%4024" target="_blank">Icon: Person</a></li>
+    <li><a href="https://fonts.google.com/icons?selected=Material%20Symbols%20Outlined%3Avisibility%3AFILL%400%3Bwght%40400%3BGRAD%400%3Bopsz%4024" target="_blank">Icon: Visibility</a></li>
+    <li><a href="https://fonts.google.com/icons?selected=Material%20Symbols%20Outlined%3Avisibility_off%3AFILL%400%3Bwght%40400%3BGRAD%400%3Bopsz%4024" target="_blank">Icon: Visibility Off</a></li>
+</ul>

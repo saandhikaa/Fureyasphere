6039275097dd03c4929a71a30f250f3e20a46f72
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-07 08:06:40 +0700

    create layout: navigation

---
+++ shared/views/layout/navigation.php
@@ -0,0 +1,52 @@
+<header id="page-header">
+    <h1 class="app"><?= $data["page-title"] ?></h1>
+    <button type="button" class="openNav nav-button mobile"><?= isset($_SESSION["sign-in"]["username"]) ? '<span>' . strtoupper($_SESSION["sign-in"]["username"][0]) . '</span>' : (function() { readfile(__DIR__ . '/../../assets/images/icons/menu_FILL0_wght400_GRAD0_opsz24.svg'); })() ?></button>
+    <section class="desktop">
+        <?= isset($_SESSION["sign-in"]["username"]) ? '<h1>Hi, ' . $_SESSION["sign-in"]["username"] . '</h1>' : '<h1 class="nav-greeting"></h1>' ?>
+    </section>
+</header>
+
+<?= isset($data["image-path"]) ? $data["image-path"] : "" ?>
+<p class="root-path no-display"><?= BASEURL ?></p>
+
+<div class="navigation-container closeNav">
+    <nav class="navigation">
+        <header class="mobile">
+            <section>
+                <?= isset($_SESSION["sign-in"]["username"]) ? '<h1>Hi, ' . $_SESSION["sign-in"]["username"] . '</h1>' : '<h1 class="nav-greeting"></h1>' ?>
+            </section>
+            <button type="button" class="closeNav nav-button"><?php readfile(__DIR__ . '/../../assets/images/icons/close_FILL0_wght400_GRAD0_opsz24.svg') ?></button>
+            <span class="separator bottom"></span>
+        </header>
+        
+        <section class="list">
+            <ul class="main-list row">
+                <li><a href="<?= BASEURL ?>/home"><?php readfile(__DIR__ . '/../../assets/images/icons/home_FILL0_wght400_GRAD0_opsz24.svg') ?><span>Home</span></a></li>
+                <li><a href="<?= BASEURL ?>/account"><?php readfile(__DIR__ . '/../../assets/images/icons/person_FILL0_wght400_GRAD0_opsz24.svg') ?><span>Account</span></a></li>
+            </ul>
+            <ul class="app-list row">
+                <h6>Services</h6>
+                <?php foreach (App::getAppDetail() as $apps): ?>
+                    <?php foreach ($apps["class"] as $controller): ?>
+                        <li><a href="<?= BASEURL .  '/' . strtolower($controller) ?>"><?= $controller ?></a></li>
+                    <?php endforeach ?>
+                <?php endforeach ?>
+            </ul>
+        </section>
+        
+        <footer>
+            <span class="separator top"></span>
+            <ul class="row feedback-list">
+                <li><a href="">Leave a Comment</a></li>
+                <li><a href="<?= $data['issue'] ?>" target="_blank">Create an Issue on GitHub</a></li>
+            </ul>
+            <p class="copyright">&copy; <?= ME ?> <span></span>.</p>
+            <ul class="footer-list mobile">
+                <li><a href="<?= BASEURL . '/' . ($data['class'] == 'account' ? 'home' : $data['class']) ?>/about">About</a></li>
+                <li><a href="<?= BASEURL ?>/home/resources" target="_blank">Resources</a></li>
+                <li><a href="<?= BASEURL ?>/home/terms" target="_blank">Terms</a></li>
+                <li><a href="<?= BASEURL ?>/home/privacy" target="_blank">Privacy</a></li>
+            </ul>
+        </footer>
+    </nav>
+</div>

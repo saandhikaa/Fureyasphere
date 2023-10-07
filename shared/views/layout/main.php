37b9bfd8d32a5989c37a69d863e8560587fc16db
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-07 08:05:57 +0700

    refactoring, include external font, add navigation

--- shared/views/layout/main.php
+++ shared/views/layout/main.php
@@ -9,14 +9,22 @@
     <?php foreach ($data["style"] as $style): ?>
         <?= $style ?>
     <?php endforeach ?>
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500&display=swap" rel="stylesheet">
 </head>
 
-<body>
-    <main id="<?= $data["main-id"] ?>">
+<body id="body">
+    <?php if (isset($data["navigation"]) && $data["navigation"]): ?>
+        <?php require_once __DIR__ . "/navigation.php" ?>
+    <?php else: ?>
         <h1><?= $data["page-title"] ?></h1>
-        
+    <?php endif ?>
+    
+    <main id="<?= $data["main-id"] ?>">
         <?php foreach ($data["body"] as $section): ?>
-            <?php file_exists($section) ? readfile($section) : false ?>
+            <?php file_exists($section) ? require_once $section : false ?>
         <?php endforeach ?>
     </main>
     

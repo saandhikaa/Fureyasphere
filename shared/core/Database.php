9ccfeeffe834ce349829473e205b7a5a9a8f3945
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-12 13:37:40 +0700

    switch to mysqli

--- shared/core/Database.php
+++ shared/core/Database.php
@@ -1,97 +1,40 @@
 <?php
-    class Database extends Controller {
-        private $host = DB_HOST;
-        private $user = DB_USER;
-        private $pass = DB_PASS;
-        private $dbname = DB_NAME;
-        
-        private $dbh, $statement;
+    class Database {
+        public $conn;
         
         public function __construct() {
-            $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
-            $option = array(
-                PDO::ATTR_PERSISTENT => true,
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-            );
-            
+            $this->conn = null;    
             try {
-                $this->dbh = new PDO($dsn, $this->user, $this->pass, $option);
-                $this->query("USE " . $this->dbname);
-                $this->execute();
-            } catch (PDOException $e) {
-                $data = ["title" => "Database Error"];
-                
-                $errorCode = $e->getCode();
-                switch ($errorCode) {
-                    case 1049:
-                        $data["strong"] = "Error: Unknown database.";
-                        $data["normal"] = "Please check the database name, connection settings, or create the database if it does not exist.";
-                        break;
-                    case 1044:
-                        $data["strong"] = "Error: Access denied for user.";
-                        $data["normal"] = "Please check the username, password, connection settings, or the user's permissions.";
-                        break;
-                    case 2002:
-                        $data["strong"] = "Error: Connection refused.";
-                        $data["normal"] = "Please check the database server, host, port, or connection settings.";
-                        break;
-                    case 1045:
-                        $data["strong"] = "Error: Access denied for user.";
-                        $data["normal"] = "Please check the username, password, connection settings, or the user's permissions.";
-                        break;
-                    case 42000:
-                        $data["strong"] = "Error: SQL syntax error or access violation (42000).";
-                        $data["normal"] = "Please check the SQL syntax, database name, connection settings, or create the database if it does not exist.";
-                        break;
-                    default:
-                        $data["strong"] = "An error occurred:" . $e->getCode();
-                        $data["normal"] = $e->getMessage();
-                        break;
-                }
-                
-                $this->view(basename(dirname(__DIR__)), "shares/error", $data);
-                die;
+                $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+            } catch (Exception $exception) {
+                echo "Connection error: " . $exception->getMessage();
             }
+            return $this->conn;
         }
         
-        public function query ($query) {
-            $this->statement = $this->dbh->prepare($query);
+        public function fetching ($query) {
+            $result = $this->conn->query($query);
+            $rows = [];
+            while ($row = $result->fetch_assoc()) {
+                $rows[] = $row;
+            }
+            return $rows;
         }
         
-        public function bind ($param, $value, $type = null) {
-            if (is_null($type)) {
-                switch (true) {
-                    case is_int($value):
-                        $type = PDO::PARAM_INT;
-                        break;
-                    
-                    case is_bool($value):
-                        $type = PDO::PARAM_BOOL;
-                        break;
-                        
-                    case is_null($value):
-                        $type = PDO::PARAM_NULL;
-                        break;
-                        
-                    default:
-                        $type = PDO::PARAM_STR;
-                }
-            }
+        public function inserting ($query) {
+            return $this->conn->query($query);
+        }
+        
+        public function dropAndCreateTable($tableName, $createTableSQL) {
+            $dropQuery = "DROP TABLE IF EXISTS $tableName";
+            $createQuery = $createTableSQL;
             
-            $this->statement->bindValue($param, $value, $type);
+            $this->conn->query($dropQuery);
+            return $this->conn->query($createQuery);
         }
         
-        public function execute() {
-            return $this->statement->execute();
-        }
-        
-        public function result ($all = false) {
-            $this->execute();
-            return $all ? $this->statement->fetchAll(PDO::FETCH_ASSOC) : $this->statement->fetch(PDO::FETCH_ASSOC);
-        }
-        
-        public function rowCount() {
-            return $this->statement->rowCount();
+        public function closing() {
+            $this->conn->close();
         }
     }
 ?>

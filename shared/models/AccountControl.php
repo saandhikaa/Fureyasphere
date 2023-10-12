b47978b7c5dd56b0aecf41b942b9e17babecf1c2
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-12 14:50:50 +0700

    refactoring

--- shared/models/AccountControl.php
+++ shared/models/AccountControl.php
@@ -3,50 +3,38 @@
         private $db;
         private $table = "users";
         
-        public function __construct() {
-            $this->db = new Database;
+        public function __construct(Database $database) {
+            $this->db = $database;
         }
         
-        public function checkSignInInfo() {
+        public function isLoggedIn() {
             if (isset($_SESSION["sign-in"]["username"])) {
-                return !$this->checkUsername($_SESSION["sign-in"]["username"]);
+                return $this->userRegistered($_SESSION["sign-in"]["username"]);
             }
         }
         
-        public function checkUsername ($username) {
-            $query = "SELECT username_ FROM $this->table WHERE username_ = :username";
-            $this->db->query($query);
-            $this->db->bind(':username', $username);
-            
-            return empty($this->db->result());
+        public function userRegistered ($username) {
+            $result = $this->db->fetching("SELECT username_ FROM $this->table WHERE username_ = '$username'");
+            return !empty($result);
         }
         
         public function signUp ($username, $password, $level = 3) {
-            if (!$this->checkUsername($username)) {
+            if ($this->userRegistered($username)) {
                 return ($this->signIn($username, $password));
             }
             
-            // Hash the password using password_hash function
             $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+            $time = time();
             
-            $this->db->query("INSERT INTO $this->table (time_, username_, password_, level_) VALUES (:time, :username, :password, :level)");
-            $this->db->bind(':time', time());
-            $this->db->bind(':username', $username);
-            $this->db->bind(':password', $hashedPassword);
-            $this->db->bind(':level', $level);
-            $this->db->execute();
-            
-            return $this->db->rowCount() > 0;
+            return $this->db->inserting("INSERT INTO $this->table (time_, username_, password_, level_) VALUES ('$time', '$username', '$hashedPassword', '$level')");
         }
         
         public function signIn ($username, $password) {
-            $this->db->query("SELECT * FROM $this->table WHERE username_ = :username");
-            $this->db->bind(':username', $username);
-            $userData = $this->db->result();
+            $userData = $this->db->fetching("SELECT * FROM $this->table WHERE username_ = '$username'");
             
-            if ($userData && password_verify($password, $userData["password_" ])) {
-                $_SESSION["sign-in"]["username"] = $userData["username_"];
-                $_SESSION["sign-in"]["level"] = $userData["level_"];
+            if (!empty($userData) && password_verify($password, $userData[0]["password_"])) {
+                $_SESSION["sign-in"]["username"] = $userData[0]["username_"];
+                $_SESSION["sign-in"]["level"] = $userData[0]["level_"];
                 return true;
             } else {
                 return false;

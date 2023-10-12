a4d9e01de0d8504abd2606527e3fd30de1404728
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-12 20:55:45 +0700

    refactoring: fix access to database

--- dark-cloud-1/models/FileHandler.php
+++ dark-cloud-1/models/FileHandler.php
@@ -5,8 +5,8 @@
         private $table = "uploads";
         private $perseconds = 24 * 60 * 60;
         
-        public function __construct() {
-            $this->db = new Database;
+        public function __construct(Database $database) {
+            $this->db = $database;
         }
         
         public function upload() {
@@ -53,9 +53,8 @@
                         "available" => "YES"
                     ];
                     
-                    if ($this->insertDB($values) == 0 || !move_uploaded_file($accepted["tmp_name"][$i], $this->path . $files[$i]["path"])) {
-                        return ["error" => "server error"];
-                    }
+                    $this->insertDB($values);
+                    move_uploaded_file($accepted["tmp_name"][$i], $this->path . $files[$i]["path"]);
                 }
                 
                 // update existing file if there's new file for previous uploaded
@@ -72,17 +71,10 @@
         
         public function updateExistingFiles ($codename, $key, $time, $prevtime) {
             // update time_ in table database
-            $query = "UPDATE $this->table SET time_ = :newTime WHERE codename_ = :codename AND key_ = :key AND time_ != :newTime";
+            $query = "UPDATE $this->table SET time_ = '$time' WHERE codename_ = '$codename' AND key_ = '$key' AND time_ != '$time'";
             
-            $this->db->query($query);
-            $this->db->bind(':newTime', $time);
-            $this->db->bind(':codename', $codename);
-            $this->db->bind(':key', $key);
-            
-            $this->db->execute();
-            
-            // Check if any rows are updated
-            if ($this->db->rowCount() > 0) {
+            // Check if any rows are updated 
+            if ($this->db->executing($query)) {
                 $filteredFiles = [];
                 $files = scandir($this->path);
                 
@@ -101,7 +93,7 @@
                     $oldPath = $this->path . $file;
                     $newName = $time . substr($file, strpos($file, '_')); // Keep the rest of the filename after the underscore
                     $newPath = $this->path . $newName;
-                
+                    
                     rename($oldPath, $newPath);
                 }
             }
@@ -109,32 +101,16 @@
         
         // insert values into database
         public function insertDB ($data) {
-            $query = "INSERT INTO $this->table (time_, owner_, codename_, key_, filename_, filesize_, duration_, available_) VALUES (:time, :owner, :codename, :key, :filename, :filesize, :duration, :available)";
+            $query = "INSERT INTO $this->table (time_, owner_, codename_, key_, filename_, filesize_, duration_, available_) VALUES ('{$data["time"]}', '{$data["owner"]}', '{$data["codename"]}', '{$data["key"]}', '{$data["filename"]}', '{$data["filesize"]}', '{$data["duration"]}', '{$data["available"]}')";
             
-            $this->db->query($query);
-            $this->db->bind(':time', $data['time']);
-            $this->db->bind(':owner', $data['owner']);
-            $this->db->bind(':codename', $data['codename']);
-            $this->db->bind(':key', $data['key']);
-            $this->db->bind(':filename', $data['filename']);
-            $this->db->bind(':filesize', $data['filesize']);
-            $this->db->bind(':duration', $data['duration']);
-            $this->db->bind(':available', $data['available']);
-            
-            $this->db->execute();
-            
-            return $this->db->rowCount();
+            return $this->db->executing($query);
         }
         
         // update availability file = NO and remove file that passed the limit
         public function autoRemove() {
             // get list of file that passed the limit
-            $currentTime = time();
-            $query = "SELECT codename_, key_, time_, filename_ FROM $this->table WHERE available_ = 'YES' AND time_ < :currentTime - (duration_ * :perseconds)";
-            $this->db->query($query);
-            $this->db->bind(':currentTime', $currentTime);
-            $this->db->bind(':perseconds', $this->perseconds);
-            $result = $this->db->result(true);
+            $limit = time() - $this->perseconds;
+            $result = $this->db->fetching("SELECT codename_, key_, time_, filename_ FROM $this->table WHERE available_ = 'YES' AND time_ < '$limit')");
             
             // remove file
             foreach ($result as $entry) {
@@ -150,11 +126,7 @@
             }
             
             // update VALUES at column available_ to NO
-            $query = "UPDATE $this->table SET available_ = 'NO' WHERE time_ < :currentTime - (duration_ * :perseconds)";
-            $this->db->query($query);
-            $this->db->bind(':currentTime', $currentTime);
-            $this->db->bind(':perseconds', $this->perseconds);
-            $this->db->execute();
+            $this->db->executing("UPDATE $this->table SET available_ = 'NO' WHERE time_ < '$limit'");
         }
         
         function download ($savedName, $filePath) {
@@ -167,9 +139,9 @@
             header('Expires: 0');
             header('Cache-Control: must-revalidate');
             header('Pragma: public');
-        
+            
             flush();
-        
+            
             readfile($fullPath);
         }
         
@@ -186,20 +158,8 @@
             }
         }
         
-        // get list of file by codename and key
-        public function loadFiles ($codename, $key) {
-            $query = "SELECT * FROM $this->table WHERE codename_ = :codename AND key_ = :key AND available_ = 'YES'";
-            $this->db->query($query);
-            $this->db->bind(':codename', $codename);
-            $this->db->bind(':key', $key);
-            return $this->db->result(true);
-        }
-        
         public function generateKey ($codename) {
-            $query = "SELECT key_ FROM $this->table WHERE codename_ = :codename AND available_ = 'YES' ";
-            $this->db->query($query);
-            $this->db->bind(':codename', $codename);
-            $result = $this->db->result(true);
+            $result = $this->db->fetching("SELECT key_ FROM $this->table WHERE codename_ = '$codename' AND available_ = 'YES'");
             
             $keys = array_column($result, 'key_');
             $keys = array_unique($keys);
@@ -245,10 +205,7 @@
         
         // handling for submitted form, check for codename and file
         public function handleRePost ($codename, $files) {
-            $query = "SELECT filename_, key_, time_ FROM $this->table WHERE codename_ = :codename AND available_ = 'YES' ";
-            $this->db->query($query);
-            $this->db->bind(':codename', $codename);
-            $result = $this->db->result(true);
+            $result = $this->db->fetching("SELECT filename_, key_, time_ FROM $this->table WHERE codename_ = '$codename' AND available_ = 'YES'");
             
             $time = [];
             $listed = [];
@@ -280,7 +237,6 @@
         
         public function createUploadsDir() {
             $directoryPath = $this->path;
-            $result = '';
             
             if (is_dir($directoryPath)) {
                 $files = glob($directoryPath . '/*');
@@ -290,16 +246,14 @@
                     }
                 }
                 rmdir($directoryPath);
-                $result .= "<p>Uploads directory deleted.</p>" . PHP_EOL;
             }
             
-            if (mkdir($directoryPath, 0777, true)) {
-                $result .= "<p>Uploads directory created.</p>" . PHP_EOL;
-            } else {
-                $result .= "<p>Failed to create uploads directory.</p>" . PHP_EOL;
-            }
-            
-            return $result;
+            mkdir($directoryPath, 0777, true);
+        }
+        
+        // get list of file by codename and key
+        public function loadFiles ($codename, $key) {
+            return $this->db->fetching("SELECT * FROM $this->table WHERE codename_ = '$codename' AND key_ = '$key' AND available_ = 'YES'");
         }
         
         public static function formatBytes($num_bytes) {

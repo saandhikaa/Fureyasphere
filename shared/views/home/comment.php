78a20b355f1393bb4541cb2f220cca49d3d2b42d
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-17 20:35:43 +0700

    create comment page

---
+++ shared/views/home/comment.php
@@ -0,0 +1,10 @@
+<main id="comment">
+    <form action="" method="post">
+        <label for="comment-message">Comment</label>
+        <input type="text" id="comment-message" name="message">
+        
+        <input type="hidden" name="mentioned" value="0">
+        
+        <input type="submit" name="submit" value="send">
+    </form>
+</main>

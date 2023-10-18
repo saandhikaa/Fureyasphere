6e05b6dada16c8a718adc5f5d5c925dd00850911
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-18 21:48:29 +0700

    create method loadComments & some refactoring

--- shared/models/HomeFunction.php
+++ shared/models/HomeFunction.php
@@ -1,24 +1,42 @@
 <?php
     class HomeFunction {
-        private $dataDir, $commentPath;
+        private $db;
+        private $dataDir, $commentsPath;
         
-        public function __construct() {
+        public function __construct(Database $database) {
             $this->dataDir = __DIR__ . "/../data-userfilesbox";
-            $this->commentPath = $this->dataDir . "/comments.json";
+            $this->commentsPath = $this->dataDir . "/comments.json";
             
             if (!is_dir($this->dataDir)) {
                 mkdir($this->dataDir, 0777, true);
             }
-            if (!file_exists($this->commentPath)) {
-                file_put_contents($this->commentPath, json_encode([]));
+            if (!file_exists($this->commentsPath)) {
+                file_put_contents($this->commentsPath, json_encode([]));
             }
+            
+            $this->db = $database;
         }
         
-        public function addComment($newComment) {
-            $comments = json_decode(file_get_contents($this->commentPath), true);
+        public function addComment ($newComment) {
+            $comments = json_decode(file_get_contents($this->commentsPath), true);
             $comments[] = $newComment;
             
-            return file_put_contents($this->commentPath, json_encode($comments));
+            return file_put_contents($this->commentsPath, json_encode($comments));
+        }
+        
+        public function loadComments() {
+            $comments = json_decode(file_get_contents($this->commentsPath), true);
+            
+            $rows = [];
+            foreach ($comments as $comment) {
+                $rows[] = [
+                    "time" => date('M d, Y', $comment['time']),
+                    "username" => $this->db->fetching("SELECT username_ FROM users WHERE time_ = {$comment['uid']}")[0]["username_"],
+                    "message" => $comment['message']
+                ];
+            }
+            
+            return $rows;
         }
     }
 ?>

36f9db562bd0fd860c9612ca17d49a9458accd54
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-18 21:49:06 +0700

    create comment list

--- shared/views/home/comment.php
+++ shared/views/home/comment.php
@@ -1,4 +1,10 @@
 <main id="comment">
+    <?php foreach ($data["comment-lists"] as $comment): ?>
+        <p><strong><?= $comment["username"] ?></strong> <span class="time"><?= $comment["time"] ?></span></p>
+        <p><?= $comment["message"] ?></p>
+        <br>
+    <?php endforeach ?>
+    
     <form action="" method="post">
         <label for="comment-message">Comment</label>
         <input type="text" id="comment-message" name="message">

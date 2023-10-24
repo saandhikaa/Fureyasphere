dcc0ad97b949c66c60542384db4fd4ab4478728d
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-24 22:42:03 +0700

    add confirmation before delete account

--- shared/views/account/index.php
+++ shared/views/account/index.php
@@ -4,7 +4,9 @@
     <?php if ($_SESSION["sign-in"]["username"] === ADMIN_USERNAME): ?>
         <a href="<?= BASEURL . '/' . $data['class'] ?>/setup">Setup</a>
     <?php else: ?>
-        <form action="<?= BASEURL . '/' . $data['class'] ?>/delete" method="post"><input type="submit" value="Deleted my account"></form>
+        <form action="<?= BASEURL . '/' . $data['class'] ?>/delete" method="post" onsubmit="return confirm('Are you sure you want to delete your account?');">
+            <input type="submit" value="Delete my account">
+        </form>
     <?php endif ?>
     
     <a href="<?= BASEURL . '/' . $data['class'] ?>/signout">Sign Out</a>

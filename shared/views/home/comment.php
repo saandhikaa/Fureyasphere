548ea3509dfdd436f50b6523e7b6b716270604bf
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-24 22:42:35 +0700

    refactoring: comment page structure

--- shared/views/home/comment.php
+++ shared/views/home/comment.php
@@ -1,20 +1,34 @@
 <main id="comment">
-    <?php foreach ($data["comment-lists"] as $comment): ?>
-        <p><strong><?= $comment["username"] ?></strong> <span class="time"><?= $comment["time"] ?></span></p>
-        <p><?= $comment["message"] ?></p>
-        <br>
-    <?php endforeach ?>
+    <ul class="comment-root">
+        <?php foreach ($data["comment-lists"] as $comment): ?>
+            <li>
+                <section>
+                    <p><strong><?= $comment["username"] ?></strong> <span class="time"><?= $comment["time"] ?></span></p>
+                    <p><?= $comment["message"] ?></p>
+                    <ul class="comment-action">
+                        <li><button>Like</button></li>
+                        <li><button>Reply</button></li>
+                        <li><button>Show replies</button></li>
+                    </ul>
+                </section>
+            </li>
+        <?php endforeach ?>
+    </ul>
     
-    <?php if (isset($_SESSION["sign-in"])): ?>
-        <form action="" method="post">
-            <label for="comment-message">Comment</label>
-            <input type="text" id="comment-message" name="message">
-            
-            <input type="hidden" name="mentioned" value="0">
-            
-            <input type="submit" name="submit" value="send">
-        </form>
-    <?php else: ?>
-        <a href="<?= BASEURL . '/account/signin' ?>">Sign in to Comment</a>
-    <?php endif ?>
+    <section class="add-comment">
+        <?php if (isset($_SESSION["sign-in"])): ?>
+            <form action="" method="post">
+                <input type="hidden" name="replied" value="0">
+                
+                <section class="field">
+                    <label for="comment-message">Comment</label>
+                    <input type="text" id="comment-message" name="message">
+                </section>
+                
+                <input type="submit" name="submit" value="send">
+            </form>
+        <?php else: ?>
+            <a href="<?= BASEURL . '/account/signin' ?>" class="redirect">Sign in to Comment</a>
+        <?php endif ?>
+    </section>
 </main>

369e2f8fd78d3a4ab7efb66b5e6b27ea8a2f12e5
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-24 21:58:58 +0700

    update parameter for database handler: queries & params

--- shared/models/AccountControl.php
+++ shared/models/AccountControl.php
@@ -9,13 +9,13 @@
         
         public function isLoggedIn() {
             if (isset($_SESSION["sign-in"]["uid"])) {
-                $userData = $this->db->fetching("SELECT * FROM $this->table WHERE time_ = '{$_SESSION["sign-in"]["uid"]}'");
+                $userData = $this->db->fetching("SELECT * FROM $this->table WHERE time_ = ?", [$_SESSION["sign-in"]["uid"]]);
                 return !empty($userData[0]) ? $userData[0] : false ;
             }
         }
         
         public function userRegistered ($username) {
-            $result = $this->db->fetching("SELECT username_ FROM $this->table WHERE username_ = '$username'");
+            $result = $this->db->fetching("SELECT * FROM $this->table WHERE username_ = ?", [$username]);
             return !empty($result);
         }
         
@@ -29,12 +29,13 @@
             if (! $agreement_accepted = isset($data["agreement"]) ? 1 : 0 ) {
                 return false;
             }
-            
-            return $this->db->executing("INSERT INTO $this->table (time_, username_, password_, level_, agreed_) VALUES ($time, '{$data["username"]}', '$hashedPassword', $level, $agreement_accepted)");
+            $query = "INSERT INTO $this->table (time_, username_, password_, level_, agreed_) VALUES (?, ?, ?, ?, ?)";
+            $params = [$time, $data["username"], $hashedPassword, $level, $agreement_accepted];
+            return $this->db->executing($query, $params);
         }
         
         public function signIn ($username, $password) {
-            $userData = $this->db->fetching("SELECT * FROM $this->table WHERE username_ = '$username'");
+            $userData = $this->db->fetching("SELECT * FROM $this->table WHERE username_ = ?", [$username]);
             
             if (!empty($userData) && password_verify($password, $userData[0]["password_"])) {
                 $_SESSION["sign-in"]["username"] = $userData[0]["username_"];
@@ -46,7 +47,7 @@
         }
         
         public function dropUser() {
-            return $this->db->executing("DELETE FROM $this->table WHERE time_ = {$_SESSION['sign-in']['uid']}");
+            return $this->db->executing("DELETE FROM $this->table WHERE time_ = ?", [$_SESSION['sign-in']['uid']]);
         }
     }
 ?>

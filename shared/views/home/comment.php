2dc1018df0080abc9520a8a1f39bb3f194d97a89
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-28 17:03:47 +0700

    update page: comment: add some attribute

--- shared/views/home/comment.php
+++ shared/views/home/comment.php
@@ -5,9 +5,9 @@
                 <p><strong><?= $comment["username"] ?></strong> <span class="time"><?= $comment["time"] ?></span></p>
                 <p><?= $comment["message"] ?></p>
                 <ul class="comment-action">
-                    <li><button>Like</button></li>
-                    <li><button>Reply</button></li>
-                    <li><button>Show replies</button></li>
+                    <li><button class="likeThis" data-commentId="<?= $comment["id"] ?>">Like</button></li>
+                    <li><button class="replyThis" data-commentId="<?= $comment["id"] ?>">Reply</button></li>
+                    <li><button class="showReplies" data-commentId="<?= $comment["id"] ?>">Show replies</button></li>
                 </ul>
             </li>
         <?php endforeach ?>
@@ -16,11 +16,11 @@
     <section class="add-comment">
         <?php if (isset($_SESSION["sign-in"])): ?>
             <form action="" method="post">
-                <input type="hidden" name="replied" value="0">
+                <input id="reply" type="hidden" name="replied" value="0">
                 
                 <section class="field">
-                    <label for="comment-message">Comment</label>
-                    <input type="text" id="comment-message" name="message">
+                    <label for="comment-message">Comment as <?= $_SESSION["sign-in"]["username"] ?></label>
+                    <textarea id="comment-message" name="message" rows="1"></textarea>
                 </section>
                 
                 <input type="submit" name="submit" value="send">

f3a527f73d70cdfce70207b4f39c400ad7a9399c
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-10-28 21:20:57 +0700

    update page: comment: show replies

--- shared/views/home/comment.php
+++ shared/views/home/comment.php
@@ -9,6 +9,20 @@
                     <li><button class="replyThis" data-commentId="<?= $comment["id"] ?>">Reply</button></li>
                     <li><button class="showReplies" data-commentId="<?= $comment["id"] ?>">Show replies</button></li>
                 </ul>
+                <?php if (!empty($comment["replies"])): ?>
+                    <ul class="comment-child">
+                        <?php foreach ($comment["replies"] as $reply): ?>
+                            <li class="comment-field">
+                                <p><strong><?= $reply["username"] ?></strong> <span class="time"><?= $reply["time"] ?></span></p>
+                                <p><?= $reply["message"] ?></p>
+                                <ul class="comment-action">
+                                    <li><button class="likeThis" data-commentId="<?= $reply["id"] ?>">Like</button></li>
+                                    <li><button class="mentionThis" data-commentId="<?= $reply["id"] ?>">Reply</button></li>
+                                </ul>
+                            </li>
+                        <?php endforeach ?>
+                    </ul>
+                <?php endif ?>
             </li>
         <?php endforeach ?>
     </ul>
@@ -19,7 +33,7 @@
                 <input id="reply" type="hidden" name="replied" value="0">
                 
                 <section class="field">
-                    <label for="comment-message">Comment as <?= $_SESSION["sign-in"]["username"] ?></label>
+                    <label for="comment-message"><span class="status">Comment</span> as <?= $_SESSION["sign-in"]["username"] ?></label>
                     <textarea id="comment-message" name="message" rows="1"></textarea>
                 </section>
                 

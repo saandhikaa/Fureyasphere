bead93277d2561fc73d33ac2fe1c006d78d13b49
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-11-04 23:26:19 +0700

    fix Scanning new reply

--- shared/assets/js/appHome.js
+++ shared/assets/js/appHome.js
@@ -7,3 +7,14 @@ Scanning.prototype.newComment = element => {
         }
     }
 };
+
+Scanning.prototype.newReply = element => {
+    if (element.getAttribute('data-isLoggedIn')) {
+        document.querySelector('#comment').classList.add('show');
+        document.querySelector('input#reply').value = element.getAttribute('data-commentId');
+    } else {
+        if (window.confirm('Access denied, sign in needed.\n\nDo you wish to proceed to the sign-in page?')) {
+            window.location.href = document.querySelector('.root-path').textContent + '/account/signin/home/comment';
+        }
+    }
+};

48ce45fc9a6a6ef7a6b6bf0454114204600fe24c
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-11-04 23:18:18 +0700

    update page: comment: comment list structure

--- shared/views/home/comment.php
+++ shared/views/home/comment.php
@@ -1,49 +1,34 @@
 <main id="comment">
     <h1>Comment section</h1>
-    <ul class="comment-list-level1">
+    
+    <ul class="comment-list root">
         <?php foreach ($data["comment-lists"] as $comment): ?>
-            <li class="level1">
-                <section class="comment-container">
-                    <p><strong><?= $comment["username"] ?></strong> <span class="time"><?= $comment["time"] ?></span></p>
-                    <p><?= $comment["message"] ?></p>
-                    <ul class="comment-action">
-                        <li><button class="likeThis" data-commentId="<?= $comment["id"] ?>">Like</button></li>
-                        <li><button class="replyThis" data-commentId="<?= $comment["id"] ?>">Reply</button></li>
-                        <li><button class="showReplies" data-commentId="<?= $comment["id"] ?>">Show replies</button></li>
-                    </ul>
-                </section>
+            <li>
+                <p class="username"><strong><?= $comment["username"] ?></strong> <span class="time"><?= $comment["time"] ?></span></p>
+                <p class="message"><?= $comment["message"] ?></p>
+                <ul class="comment-action">
+                    <li><button class="likeThis" data-commentId="<?= $comment["id"] ?>" data-isLoggedIn="<?= isset($_SESSION["sign-in"]) ? $_SESSION["sign-in"]["username"] : false ?>">Like</button></li>
+                    <li><button class="newReply" data-commentId="<?= $comment["id"] ?>" data-isLoggedIn="<?= isset($_SESSION["sign-in"]) ? $_SESSION["sign-in"]["username"] : false ?>">Reply</button></li>
+                    <li><button class="showReplies" data-commentId="<?= $comment["id"] ?>">Show replies</button></li>
+                </ul>
                 
-                <?php if (!empty($comment["replies"])): ?>
-                    <ul class="comment-list-level2">
+                <section class="comment-replies">
+                    <ul class="comment-list">
                         <?php foreach ($comment["replies"] as $reply): ?>
-                            <li class="comment-container level2">
-                                <p><strong><?= $reply["username"] ?></strong> <span class="time"><?= $reply["time"] ?></span></p>
-                                <p><?= $reply["message"] ?></p>
+                            <li>
+                                <p class="username"><strong><?= $reply["username"] ?></strong> <span class="time"><?= $reply["time"] ?></span></p>
+                                <p class="message"><?= $reply["message"] ?></p>
                                 <ul class="comment-action">
-                                    <li><button class="likeThis" data-commentId="<?= $reply["id"] ?>">Like</button></li>
-                                    <li><button class="mentionThis" data-commentId="<?= $reply["id"] ?>">Reply</button></li>
+                                    <li><button class="likeThis" data-commentId="<?= $reply["id"] ?>" data-isLoggedIn="<?= isset($_SESSION["sign-in"]) ? $_SESSION["sign-in"]["username"] : false ?>">Like</button></li>
+                                    <li><button class="newReplyMention" data-commentId="<?= $reply["id"] ?>" data-isLoggedIn="<?= isset($_SESSION["sign-in"]) ? $_SESSION["sign-in"]["username"] : false ?>">Reply</button></li>
                                 </ul>
                             </li>
                         <?php endforeach ?>
                     </ul>
-                <?php endif ?>
+                </section>
             </li>
         <?php endforeach ?>
     </ul>
     
-    <section class="add-comment">
-        <?php if (isset($_SESSION["sign-in"])): ?>
-            <form action="" method="post">
-                <input id="reply" type="hidden" name="replied" value="0">
-                
-                <label for="comment-message"><span class="status">Comment</span> as <?= $_SESSION["sign-in"]["username"] ?></label>
-                <section class="field">
-                    <textarea id="comment-message" name="message" rows="1" placeholder="Write a comment (<b> and <i> tags available)"></textarea>
-                    <input type="submit" name="submit" value="send">
-                </section>
-            </form>
-        <?php else: ?>
-            <a href="<?= BASEURL . '/account/signin' ?>" class="redirect">Sign in to Comment</a>
-        <?php endif ?>
-    </section>
+    <button class="newComment" data-isLoggedIn="<?= isset($_SESSION["sign-in"]) ? $_SESSION["sign-in"]["username"] : false ?>">Add comment</button>
 </main>

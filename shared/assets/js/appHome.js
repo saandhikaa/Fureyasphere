f027a78d4e9eae626cb7412828bb2e5f3b7888af
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-11-04 23:25:13 +0700

    fix Scanning new comment

--- shared/assets/js/appHome.js
+++ shared/assets/js/appHome.js
@@ -1,18 +1,9 @@
-commentMSG = document.querySelector('#comment-message');
-if (commentMSG) {
-    commentMSG.addEventListener('blur', () => {
-        const selectedElements = document.querySelectorAll(".selected");
-        selectedElements.forEach(element => {
-            element.classList.remove("selected");
-        }); 
-    });
-}
-
-Scanning.prototype.replyThis = element => {
-    document.querySelector('input#reply').value = element.getAttribute('data-commentId');
-    document.querySelector('form label span').textContent = "Reply";
-    
-    element.closest('.comment-container').classList.add('selected');
-    
-    commentMSG.focus();
+Scanning.prototype.newComment = element => {
+    if (element.getAttribute('data-isLoggedIn')) {
+        document.querySelector('#comment').classList.add('show');
+    } else {
+        if (window.confirm('Access denied, sign in needed.\n\nDo you wish to proceed to the sign-in page?')) {
+            window.location.href = document.querySelector('.root-path').textContent + '/account/signin/home/comment';
+        }
+    }
 };

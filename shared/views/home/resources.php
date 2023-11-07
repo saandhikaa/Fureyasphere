022442d4c746f261af16ef2386df3e9b13c978d4
gilang sandhika putra <74443723+saandhikaa@users.noreply.github.com>
2023-11-07 21:42:16 +0700

    update resources: change font to Fira sans

--- shared/views/home/resources.php
+++ shared/views/home/resources.php
@@ -1,6 +1,6 @@
 <?php
     $list["google-fonts"][] = '
-        <li><a href="https://fonts.google.com/share?selection.family=Ubuntu:wght@300;400;500;700" target="_blank">Font family: Ubuntu</a></li>
+        <li><a href="https://fonts.google.com/share?selection.family=Fira+Sans:wght@200;300;400;500;600;700;800;900" target="_blank">Font family: Fira sans</a></li>
         <li><a href="https://fonts.google.com/icons?selected=Material%20Symbols%20Outlined%3Aarrow_upward%3AFILL%400%3Bwght%40400%3BGRAD%400%3Bopsz%4024" target="_blank">Icon: Arrow Upward</a></li>
         <li><a href="https://fonts.google.com/icons?selected=Material%20Symbols%20Outlined%3Acheck_circle%3AFILL%400%3Bwght%40400%3BGRAD%400%3Bopsz%4024" target="_blank">Icon: Check Circle</a></li>
         <li><a href="https://fonts.google.com/icons?selected=Material%20Symbols%20Outlined%3Aclose%3AFILL%400%3Bwght%40400%3BGRAD%400%3Bopsz%4024" target="_blank">Icon: Close</a></li>
